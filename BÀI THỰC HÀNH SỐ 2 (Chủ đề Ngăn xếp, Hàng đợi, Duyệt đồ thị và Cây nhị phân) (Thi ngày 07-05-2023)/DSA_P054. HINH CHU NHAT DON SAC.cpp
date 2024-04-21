263867beb6a7a02a0ef215ec31200e9cbbae0a9f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-04-21 09:01:54 +0700

    Update DSA_P054. HINH CHU NHAT DON SAC.cpp

--- BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P054. HINH CHU NHAT DON SAC.cpp
+++ BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P054. HINH CHU NHAT DON SAC.cpp
@@ -2,29 +2,27 @@
 using namespace std;
 
 long long kq(long long a[], long long n){
-	long long res = -1e18;
-	stack<long long>st; //luu chi so cua cot hinh chu nhat
-	long long i = 0;
-	while(i<n){
-		if(st.empty()||a[i]>a[st.top()]){ //co the noi dai duoc them hinh chu nhat
-			st.push(i);
-			i++;
-		} else { //Hinh chu nhat loang den i thi bi dut doan
-			long long idx = st.top();
-			st.pop();
-			//Can phai la i, can trai la cai truoc idx strong st
-			if(st.empty()) res = max(res, i * a[idx]);
-			else res = max(res, a[idx] * (i - st.top()-1));
-		}
-	}
-	while(!st.empty()){
-		long long idx = st.top();
-		st.pop();
-		//Can phai la i, can trai la cai truoc idx strong st
-		if(st.empty()) res = max(res, i * a[idx]);
-		else res = max(res, a[idx] * (i - st.top()-1));
-	}
-	return res;
+        long long l[n], r[n];, res = -1e18;
+        // Tìm Vị trí phần tử đầu tiên bên trái, bên phải a[i] bé hơn a[i]
+        //l, r là cận trái, phải của HCN
+        for(long long i = 0;i<n;i++) cin>>a[i];
+        stack<long long>st1, st2;
+        for(long long i = n - 1;i>=0;i--){
+            while(!st1.empty() && a[i] <= a[st1.top()]) st1.pop();
+            if(st1.empty()) r[i] = n - 1;
+            else r[i] = st1.top() - 1;
+            st1.push(i);
+        }
+        for(long long i = 0;i<n;i++){
+            while(!st2.empty() && a[i] <= a[st2.top()]) st2.pop();
+            if(st2.empty()) l[i] = 0;
+            else l[i] = st2.top() + 1;
+            st2.push(i);
+        }
+        long long res = -1;
+        for(long long i = 0;i<n;i++){
+            res = max(res, 1LL * (r[i] - l[i] + 1) * a[i]);
+        }
 }
 
 int main(){
