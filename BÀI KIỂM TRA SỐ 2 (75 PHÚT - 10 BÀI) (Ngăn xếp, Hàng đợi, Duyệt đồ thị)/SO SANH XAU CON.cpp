03c008cb0177c783381a97eae1acbd0fb8ba7c7f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-05-07 18:25:02 +0700

    Add files via upload

---
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/SO SANH XAU CON.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s << endl;
+    s = " " + s;
+    int q;
+    cin >> q;
+    int n = s.size();
+    
+    vector<vector<int>> a(26, vector<int>(n, 0)); // 26 là số lượng ký tự chữ cái trong tiếng Anh
+    
+    unordered_map<char, int> count;
+    for (int j = 1; j <= n; j++) {
+        count[s[j] - 'a']++;
+        for (int i = 0; i < 26; i++) a[i][j] = count[i];
+    }
+    
+    while(q--){
+        int x, y, z, t;
+        cin>>x>>y>>z>>t;
+        int check = 1;
+        for(int i = 0;i<26;i++){
+            if(a[i][y] - a[i][x - 1] != a[i][t] - a[i][z - 1]){
+                check = 0;
+                break;
+            }
+        }
+        if(check ==1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
