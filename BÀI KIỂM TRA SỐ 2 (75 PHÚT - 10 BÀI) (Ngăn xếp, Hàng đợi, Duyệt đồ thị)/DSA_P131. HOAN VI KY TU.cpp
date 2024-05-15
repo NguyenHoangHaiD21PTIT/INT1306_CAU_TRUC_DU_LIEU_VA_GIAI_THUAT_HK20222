3ac05bcb47fd5f0b812d22e634dcb44cf69f691c
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-05-15 09:14:13 +0700

    Add files via upload

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DSA_P131. HOAN VI KY TU.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DSA_P131. HOAN VI KY TU.cpp
@@ -1,17 +1,15 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
+int main(){
     char x, y;
-    cin >> x >> y;
-    if (x > y)swap(x, y);
+    cin>>x>>y;
+    if(x>y) swap(x, y);
     vector<char>v;
     for(char i = x;i<=y;i++) v.push_back(i);
-    string s;
     for(char x: v) cout<<x;
     cout<<endl;
     while(next_permutation(v.begin(), v.end())){
         for(char x: v) cout<<x;
-        cout<<endl;    
+        cout<<endl;
     }
-    return 0;
 }
