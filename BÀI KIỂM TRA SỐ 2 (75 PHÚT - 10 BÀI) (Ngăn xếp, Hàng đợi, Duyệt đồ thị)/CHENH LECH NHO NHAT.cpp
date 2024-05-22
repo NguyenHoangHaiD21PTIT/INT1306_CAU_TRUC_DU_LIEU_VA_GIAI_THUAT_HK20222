348216b7b70498dd030c5dee8a17ee222adcc739
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-05-22 21:53:09 +0700

    Update CHENH LECH NHO NHAT.cpp

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/CHENH LECH NHO NHAT.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/CHENH LECH NHO NHAT.cpp
@@ -6,26 +6,22 @@ int main(){
 	int a[n] ; 
 	for(auto &x : a) cin >> x; 
 	sort(a , a + n , greater<int>()) ;
-    //10 7 5 3 2 1
 	ll sum = 0 ; 
 	vector<int> v ; 
 	map<int,int> used ;
-	int i = k ; 
 	int j = 0 ; 
-	while(i --){
+	for(int i = 0;i<k;i++){
 		v.push_back(a[j]) ;
-		used[ j ] = 1 ; 
+		used[j] = 1 ; 
 		j += 2 ; 
 	}
-	for(i = k ; i < n ; i ++){
+	for(int i = 0; i < n; i ++){
 		if(used[i] == 0){
 			v.push_back(a[i]) ;
 			used[i] = 1 ; 
 		}
 	}
-	for(int i = 0 ; i < n - k ; i ++){
-		sum += abs(v[i] - v[i + k]); 
-	}
+	for(int i = 0; i < n - k; i ++) sum += abs(v[i] - v[i + k]); 
 	cout << sum << endl ;
 	return 0 ; 
 }/*
