f709e3d1a44211660e97f358b70b872bd549a0cc
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-05-23 10:30:10 +0700

    Update DSA_P138. GIA TRI NHO NHAT.cpp

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DSA_P138. GIA TRI NHO NHAT.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DSA_P138. GIA TRI NHO NHAT.cpp
@@ -1,16 +1,19 @@
 #include<bits/stdc++.h>
-using namespace std ; 
+using namespace std;
 int main(){
-	int n , k ; cin >> n >> k ; 
-	int a[n] ; 
-	for(auto &x : a) cin >> x; 
-	sort(a , a + n , greater<int>()) ;
-	int sum = 0; 
-	vector<int> v ; 
-    for(int i = 0;i<k;i++) v.push_back(a[i*2]);
-    for(int i = 0;i<k;i++) v.push_back(a[i*2 + 1]);
-    for(int i = 2 * k;i<n;i++) v.push_back(a[i]);
-    for(int i =0;i<n - k;i++) sum+=abs(v[i] - v[i + k]);
-    cout<<sum<<endl;
+    int n, k;
+    cin>>n>>k;
+    int a[n + 1];
+    for(int i = 1;i<=n;i++) cin>>a[i];
+    sort(a + 1, a + n + 1);
+    int ans = 0, pos = n/k + 1;
+    a[0] = a[1];
+    for(int i = 1;i<=n;i++){
+        if(i==pos){
+            pos+=n/k;
+            continue;
+        }
+        ans+=a[i] - a[i - 1];
+    }
+    cout<<ans<<endl;
 }
-//BÀI NÀY mới 4/10
