b6dbac55aeb28ce37b0ab473bc9d006800741835
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-05-23 11:23:07 +0700

    Update DSA_P139. SO LAN DUYET IT NHAT.cpp

--- BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P139. SO LAN DUYET IT NHAT.cpp
+++ BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P139. SO LAN DUYET IT NHAT.cpp
@@ -1,95 +1,34 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+vector<int>ke[1005];
+int visited[1005] = {};
 
-const int MAXN = 1001;
-vector<int> graph[MAXN], reGraph[MAXN];
-bool visited[MAXN];
-stack<int> fStack;
-vector<vector<int>> sccs;
-
-void dfs1(int node) {
-    visited[node] = true;
-    for (int nei : graph[node]) {
-        if (!visited[nei]) {
-            dfs1(nei);
-        }
-    }
-    fStack.push(node);
-}
-
-void dfs2(int node, vector<int>& currentScc) {
-    visited[node] = true;
-    currentScc.push_back(node);
-    for (int nei : reGraph[node]) {
-        if (!visited[nei]) {
-            dfs2(nei, currentScc);  
-        }
+void DFS(int x){
+    visited[x] = 1;
+    for(int i: ke[x]){
+        if(!visited[i]) DFS(i);
     }
 }
-
-int findSccs(int n) {
-    
-    memset(visited, false, sizeof(visited));
-    for (int i = 1; i <= n; ++i) {
-        if (!visited[i]) {
-            dfs1(i);
-        }
+int main(){
+    int dinh, canh;
+    cin>>dinh>>canh;
+    while(canh--){
+        int x, y;
+        cin>>x>>y;
+        ke[x].push_back(y);
     }
-
-    
-    memset(visited, false, sizeof(visited));
-    while (!fStack.empty()) {
-        int node = fStack.top();
-        fStack.pop();
-        if (!visited[node]) {
-            vector<int> currentScc;
-            dfs2(node, currentScc);  
-            sccs.push_back(currentScc);
-        }
-    }
-
-   
-    int sccCount = sccs.size();
-    vector<int> sccMap(n + 1, -1);
-    for (int i = 0; i < sccCount; ++i) {
-        for (int node : sccs[i]) {
-            sccMap[node] = i;
-        }
-    }
-
-    vector<int> inDegrees(sccCount, 0);
-    for (int u = 1; u <= n; ++u) {
-        for (int v : graph[u]) {
-            if (sccMap[u] != sccMap[v]) {
-                inDegrees[sccMap[v]]++;
+    int ans = INT_MAX;
+    for(int i = 1;i<=dinh;i++){
+        int cnt = 1;
+        memset(visited, 0, sizeof(visited));
+        DFS(i);
+        for(int k = 1;k<=dinh;k++){
+            if(!visited[k]){
+                DFS(k);
+                cnt++;
             }
         }
+        ans = min(ans, cnt);
     }
-
-    int srCount = 0;
-    for (int i = 0; i < sccCount; ++i) {
-        if (inDegrees[i] == 0) {
-            srCount++;
-        }
-    }
-
-    return srCount;
-}
-
-int main() {
-    int N, M;
-    cin >> N >> M;
-
-    for (int i = 0; i < M; ++i) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        reGraph[v].push_back(u);
-    }
-
-    int result = findSccs(N);
-    cout << result << endl;
-
-    return 0;
+    cout<<ans;
 }
