d7eacf82d5d016c60e68500b8a559d7e94ca202f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-06-30 16:21:07 +0700

    Update DSA_P055. CAP SO.cpp

--- BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P055. CAP SO.cpp
+++ BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P055. CAP SO.cpp
@@ -1,28 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n, ans = 0;
-    stack<int> st;
-    map<int, int> cnt;
-    cin >> n;
-    for(int i = 1; i <= n; i++){
-        int x; cin >> x;
-        while (!st.empty() && x > st.top()){
-            cnt[st.top()]--;
-            st.pop();
-            ans++;
-        }
-        if (!st.empty()){
-            if (x == st.top()){
-               ans += cnt[x] + (st.size() > cnt[x]);
-            }
-            else ans += 1;
-        }
-        st.push(x);
-        cnt[x]++;
+// Tìm vị trí đầu tiên >= x trong mảng đã sắp xếp xét trong đoạn chỉ số[L..R]
+int lower_bound(vector<int>& arr, int L, int x) {
+    int R = arr.size() - 1;
+    while (L < R) {
+        int mid = (L + R) / 2;
+        if (arr[mid] < x) L = mid + 1;
+        else R = mid;
     }
-    cout << ans << endl;  
+    return L;
+}
+
+// Đếm số phần tử trong mảng đã sắp xếp không vượt quá giá trị x cho trước với chỉ số trong đoạn [L; R]
+int cnt(vector<int>& arr, int l, int x) {
+    if (l >= arr.size() || arr[l] > x) return 0;
+    int pos = lower_bound(arr, l, x);
+    if (arr[pos] > x) pos -= 1;
+    return pos - l + 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    stack<int> st, st1;
+    int l[n], r[n];
+
+    // Phần tử bên trái đầu tiên lớn hơn
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && a[st.top()] <= a[i]) st.pop();
+        if (st.empty()) l[i] = -1;
+        else l[i] = st.top();
+        st.push(i);
+    }
+
+    // Phần tử bên phải đầu tiên lớn hơn
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st1.empty() && a[st1.top()] <= a[i]) st1.pop();
+        if (st1.empty()) r[i] = n;
+        else r[i] = st1.top();
+        st1.push(i);
+    }
+
+    set<int>s;
+    for(int x: a) s.insert(x);
+    vector<int>z;//z lưu những phần tử tăng dần phân biệt trong a
+    for(int x: s) z.push_back(x);
+
+    //m[a[i]] lưu các index p mà a[p] = a[i] gốc lúc đầu
+    //first[a[i]]: Chỉ số đầu tiên bắt đầu xét trong mảng m[a[i]]
+    int first[z.size() + 1] = {};
+    vector<vector<int>> m(z.size());
+
+    for (int i = 0; i < n; i++) {
+        int pos = lower_bound(z, 0, a[i]);//Tìm vị trí phần tử a[i] trong mảng z
+        a[i] = pos;
+        m[a[i]].push_back(i);
+    }
+
+    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        if (l[i] != -1) res+= 1;
+        if (r[i] != n) res+= 1;
+
+        int x = cnt(m[a[i]], first[a[i]], r[i] - 1);
+        res += (x - 1) * x / 2;
+
+        first[a[i]] += x;
+    }
+
+    cout << res << endl;
+    return 0;
 }
