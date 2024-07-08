3e593d5b1cb7d6d43cbca8d20c92b7ff7fe57bad
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-08 18:33:13 +0700

    Update XAU NHI PHAN.cpp

--- LUYEN TAP SINH KE TIEP - HOC KY HE 2023.3/XAU NHI PHAN.cpp
+++ LUYEN TAP SINH KE TIEP - HOC KY HE 2023.3/XAU NHI PHAN.cpp
@@ -1,53 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int n, K, a[100], ok;
 
-void kt(){
-    for(int i = 0; i < n; i++) a[i] = 0;
+void kt() {
+    for (int i = 0; i < n; i++) a[i] = 0;
 }
 
-void sinh(){
+void sinh() {
     int i = n - 1;
-    while(i >= 0 && a[i] == 1){
+    while (i >= 0 && a[i] == 1) {
         a[i] = 0;
         i--;
     }
-    if(i == -1) ok = 0;
+    if (i == -1) ok = 0;
     else a[i] = 1;
 }
 
-bool check(){
-    int res = 0, cnt = 0;
-    for(int i = 0; i < n; i++){
-        if(a[i] == 0) cnt++;
-        else{
-            res = max(res, cnt);
+bool check() {
+    int dem[50] = {0}, cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) cnt++;
+        else {
+            if (cnt > 0) dem[cnt]++;
             cnt = 0;
         }
     }
-    res = max(res, cnt); 
-    return res == K;
+    if (cnt > 0) dem[cnt]++; 
+    return dem[K] == 1;
 }
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         cin >> n >> K;
         kt();
         ok = 1;
-        vector<string>v;
-        while(ok){
-            if(check()){
+        vector<string> v;
+        while (ok) {
+            if (check()) {
                 string s = "";
-                for(int i = 0; i < n; i++) s+= to_string(a[i]) + " ";
+                for (int i = 0; i < n; i++) s += to_string(a[i]) + " ";
                 v.push_back(s);
             }
             sinh();
         }
-        cout<<v.size()<<endl;
-        for(string x: v) cout<<x<<endl;
-        cout<<endl;
+        cout << v.size() << endl;
+        for (string x : v) cout << x << endl;
+        cout << endl;
     }
+    return 0;
 }
