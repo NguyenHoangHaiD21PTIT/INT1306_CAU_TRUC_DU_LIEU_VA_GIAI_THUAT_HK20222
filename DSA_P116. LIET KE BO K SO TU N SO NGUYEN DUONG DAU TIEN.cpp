615c5bf71808c905408d678e60f47562ce869eb7
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-13 23:04:02 +0700

    Create DSA_P116. LIET KE BO K SO TU N SO NGUYEN DUONG DAU TIEN.cpp

---
+++ DSA_P116. LIET KE BO K SO TU N SO NGUYEN DUONG DAU TIEN.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void in(vector<int> &vt) {
+    for(int x : vt) cout << x << " ";
+    cout << endl;
+}
+
+void Try(int i, int j, int n, int k, vector<int> &vt) {
+    if(i > k) {
+        in(vt);
+        return;
+    }
+    for(int x = j; x <= n; x++) {
+        vt.push_back(x);
+        Try(i+1, x, n, k, vt);
+        vt.pop_back();
+    }
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> vt;
+    Try(1, 1, n, k, vt);
+}
+
+int main() {
+    solve();
+    return 0;
+}
