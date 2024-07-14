53609812e762617e345e43a279e3fef93b2c4bd5
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-14 10:02:32 +0700

    Update DSA_P116. LIET KE BO K SO TU N SO NGUYEN DUONG DAU TIEN.cpp

--- DSA_P116. LIET KE BO K SO TU N SO NGUYEN DUONG DAU TIEN.cpp
+++ DSA_P116. LIET KE BO K SO TU N SO NGUYEN DUONG DAU TIEN.cpp
@@ -29,3 +29,32 @@ int main() {
     solve();
     return 0;
 }
+/*ĐỀ BÀI
+Cho hai số nguyên dương n và k (k ≤ n). 
+Hãy liệt kê các bộ k số từ n số nguyên dương đầu tiên thoả mãn tính chất: các số từ trái sang phải có thể trùng nhau và có thứ tự không giảm. 
+Ví dụ với n = 3 và k = 2 thì các bộ thoả mãn là: {1,1} {1,2} {1,3} {2,2} {2,3} {3,3}
+Với n = 3 và k = 3 thì ta có kết quả như trong test ví dụ. 
+
+Input
+Chỉ có 1 dòng ghi 2 số n và k (1 < k ≤ n ≤ 12).
+
+Output
+Ghi ra màn hình lần lượt các bộ k số theo mô tả đề bài, mỗi kết quả trên một dòng, các số cách nhau một khoảng trống.
+
+Ví dụ
+
+Input
+3 3
+
+Output
+1 1 1
+1 1 2
+1 1 3
+1 2 2
+1 2 3
+1 3 3
+2 2 2
+2 2 3
+2 3 3
+3 3 3
+*/
