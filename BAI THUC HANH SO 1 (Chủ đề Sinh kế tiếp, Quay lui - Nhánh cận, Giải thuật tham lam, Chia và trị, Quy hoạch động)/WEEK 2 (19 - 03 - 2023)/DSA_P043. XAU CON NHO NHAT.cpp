d91068172c30057e2cabcbac4fcf4d74ebc1dcbc
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-20 09:23:04 +0700

    Update DSA_P043. XAU CON NHO NHAT.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P043. XAU CON NHO NHAT.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P043. XAU CON NHO NHAT.cpp
@@ -7,19 +7,24 @@ int main() {
     while (t--) {
         string s;
         cin >> s;
-        int n = s.size(), k = 0, st = 0, ans = INT_MAX, x = 0, a[256] = {};
+        int n = s.size(), k = 0, st = 0, ans = INT_MAX, x = 0, a[256] = {};//st: start: điểm bắt đầu xâu con
         if (n == 1) {
             cout << 1 << endl;
             continue;
         }
         map<char, int> m;
+        //k: số ký tự phân biệt trong xâu
         for (char c : s) {
             if (!m[c]++) k++;
         }
         for (int i = 0; i < n; i++) {
-            if (++a[s[i]] == 1) x++;
+            a[s[i]]++;
+            if (a[s[i]] == 1) x++;
             if (x == k) {
-                while (a[s[st]] > 1) a[s[st++]]--;
+                while (a[s[st]] > 1) {
+                    a[s[st]] = a[s[st]] - 1;
+                    st++;
+                }
                 ans = min(ans, i - st + 1);
             }
         }
