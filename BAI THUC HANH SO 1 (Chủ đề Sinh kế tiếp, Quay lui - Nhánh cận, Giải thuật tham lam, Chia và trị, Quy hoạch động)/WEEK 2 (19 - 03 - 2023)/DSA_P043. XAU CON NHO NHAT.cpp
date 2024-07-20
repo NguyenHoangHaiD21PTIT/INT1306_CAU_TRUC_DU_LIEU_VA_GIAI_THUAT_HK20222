6ccf3998476b1cb27b2125c36b9e095567f78ca5
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-20 09:06:56 +0700

    Update DSA_P043. XAU CON NHO NHAT.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P043. XAU CON NHO NHAT.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P043. XAU CON NHO NHAT.cpp
@@ -1,45 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		string s;
-		cin>>s;
-		int n = s.size(), k = 0, st = 0, ans = 1e9, x = 0, a[256]={};
-		if(n==1)
-		{
-			cout<<1<<endl;
-			continue;
-		}
-		map<char,int>m;
-		for(int i=0;i<n;i++)
-		{
-			if(!m[s[i]])
-			{
-				m[s[i]]++;
-				k++;
-			}
-		}
-		for(int i=0;i<n;i++)
-		{
-			a[s[i]]++;
-			if(a[s[i]]==1)
-				x++;
-			if(x==k)
-			{
-				while(a[s[st]]>1)
-				{
-					if(a[s[st]]>1)
-						a[s[st]]--;
-					st++;
-				}
-				int l=i -st+1;
-				ans = min(ans,l);
-			}
-		}
-		cout<<ans<<endl;
-	}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int n = s.size(), k = 0, st = 0, ans = INT_MAX, x = 0, a[256] = {};
+        if (n == 1) {
+            cout << 1 << endl;
+            continue;
+        }
+        map<char, int> m;
+        for (char c : s) {
+            if (!m[c]++) k++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (++a[s[i]] == 1) x++;
+            if (x == k) {
+                while (a[s[st]] > 1) a[s[st++]]--;
+                ans = min(ans, i - st + 1);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
 }
