8604403244423c5d4f7f9bc0c3eee169babdac2e
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-25 22:14:17 +0700

    Update DSA_P110. TONG GIA TRI LON NHAT.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P110. TONG GIA TRI LON NHAT.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P110. TONG GIA TRI LON NHAT.cpp
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct object{
-	int wei, val;
-};
+int dp[6000][6000]={}; //dp[i][j] la GTLN cua cai tui trong luong j khi mang i do vat
 
-object a[60000];
-int dp[6000][6000];
-
-int main() {
-    int n, m;
-    cin>>n>>m;
-    for(int i=1;i<=n;i++) cin>>a[i].wei>>a[i].val;
-    int res;
-    for(int i = 1; i <= n; ++i){
-        for(int j = 1; j <= m; ++j){
-            if(j < a[i].wei) dp[i][j] = dp[i - 1][j];
-            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i].wei] + a[i].val);
-            res = max(res, dp[i][j]);
-        }
-    }
-    cout<<res;
+int main(){
+	memset(dp,0,sizeof(dp));
+	int n, s; //n: so do vat, s: khoi luong cai tui
+	cin>>n>>s;
+	int w[6000], v[6000];//w: khoi luong, v: gia tri
+	for(int i=1;i<=n;i++) cin>>w[i]>>v[i];
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=s;j++){
+			dp[i][j] = dp[i-1][j];
+			if(j>=w[i]) dp[i][j] = max (dp[i][j], dp[i-1][j-w[i]] + v[i]);
+		}
+	}
+	cout<<dp[n][s]<<endl;
 }
