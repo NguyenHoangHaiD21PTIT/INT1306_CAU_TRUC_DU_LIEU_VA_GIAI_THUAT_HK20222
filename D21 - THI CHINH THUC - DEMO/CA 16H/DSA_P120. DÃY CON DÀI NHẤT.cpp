d3b021e8475c27d361b0115b2cc069429360ca93
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-29 08:30:38 +0700

    Update DSA_P120. DÃY CON DÀI NHẤT.cpp

--- D21 - THI CHINH THUC - DEMO/CA 16H/DSA_P120. DÃY CON DÀI NHẤT.cpp
+++ D21 - THI CHINH THUC - DEMO/CA 16H/DSA_P120. DÃY CON DÀI NHẤT.cpp
@@ -13,10 +13,8 @@ int main(){
     }
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            if(a[i] * b[j - 1] == a[i - 1] * b[j]){
-                dp[i][j]=dp[i-1][j-1]+1;
-                ans=max(ans,dp[i][j]);
-            }
+            if(a[i] * b[j - 1] == a[i - 1] * b[j]) dp[i][j]=dp[i-1][j-1]+1;
+            ans=max(ans,dp[i][j]);   
         }
     }
     cout <<ans;
