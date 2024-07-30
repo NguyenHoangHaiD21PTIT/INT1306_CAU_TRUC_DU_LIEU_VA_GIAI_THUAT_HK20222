649d01ba5e9d2ede76784ddef3b090a3c5b5f045
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-30 16:10:47 +0700

    Update I. XOA DINH.cpp

--- ICPC TUAN 2/I. XOA DINH.cpp
+++ ICPC TUAN 2/I. XOA DINH.cpp
@@ -1,57 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-vector<int> parent, size;
-vector<vector<int>> adj;
-vector<bool> present;
-vector<string> result;
-
-int find(int u) {
-    if (u != parent[u]) parent[u] = find(parent[u]);
-    return parent[u];
+ 
+int par[200005], sze[200005];
+vector<int> ke[200005];
+bool present[200005];
+string result[200005];
+ 
+int find(int x) {
+    if (x == par[x]) return x;
+    return par[x] = find(par[x]);
 }
-
-void unite(int u, int v) {
-    u = find(u);
-    v = find(v);
-    if (u != v) {
-        if (size[u] < size[v]) swap(u, v);
-        parent[v] = u;
-        size[u] += size[v];
-    }
+ 
+bool Union(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    if (sze[x] < sze[y]) swap(x, y);
+    par[y] = x;
+    sze[x] += sze[y];
+    return true;
 }
-
+ 
 int main() {
-    int N, M;
-    cin >> N >> M;
-    parent.resize(N + 1);
-    size.resize(N + 1, 1);
-    adj.resize(N + 1);
-    present.resize(N + 1, false);
-    result.resize(N);
-    for (int i = 1; i <= N; ++i) parent[i] = i;
-    for (int i = 0; i < M; ++i) {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        par[i] = i;
+        sze[i] = 1;
+    }
+    for (int i = 0; i < m; ++i) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        ke[u].push_back(v);
+        ke[v].push_back(u);
     }
-    vector<int> P(N);
-    for (int i = 0; i < N; ++i) cin >> P[i];
-    int components = 0;
-    for (int i = N - 1; i >= 0; --i) {
+    vector<int> P(n);
+    for (int i = 0; i < n; ++i) cin >> P[i];
+    int components = 0;//số thành phần liên thông
+    vector<bool> is_present(n + 1, false);
+    //Chúng ta sẽ làm ngược lại: Thêm dần từng đỉnh từ cuối về
+    for (int i = n - 1; i >= 0; --i) {
         int u = P[i];
-        present[u] = true;
+        is_present[u] = true;//đã thêm vào
         components++;
-        for (int v : adj[u]) {
-            if (present[v]) {
+        for (int v : ke[u]) {
+            if (is_present[v]) {
                 if (find(u) != find(v)) {
-                    unite(u, v);
+                    Union(u, v); 
                     components--;
                 }
             }
         }
-        result[i] = (components == 1) ? "YES" : "NO";
+        if(component==1) result[i]=="YES";
+        else result[i]=="NO";
     }
-    for (int i = 0; i < N; ++i) cout << result[i] << "\n";
+    for (int i = 0; i < n; ++i) cout << result[i] << "\n";
 }
