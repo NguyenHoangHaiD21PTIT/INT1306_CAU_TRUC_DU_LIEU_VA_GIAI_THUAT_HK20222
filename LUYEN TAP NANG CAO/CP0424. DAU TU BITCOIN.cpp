4e9e3fc3cef309b25f5c944ad27966ee11307575
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-31 15:00:38 +0700

    Update CP0424. DAU TU BITCOIN.cpp

--- LUYEN TAP NANG CAO/CP0424. DAU TU BITCOIN.cpp
+++ LUYEN TAP NANG CAO/CP0424. DAU TU BITCOIN.cpp
@@ -1,46 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
-
-    // Kiểm tra nếu tất cả các số đều giống nhau
-    bool all_equal = true;
+    int res = a[0];//res: Lưu phần tử to nhất từ đầu đến index i
+    int ans = -1;//Kết quả
     for (int i = 1; i < n; ++i) {
-        if (a[i] != a[0]) {
-            all_equal = false;
-            break;
-        }
+        if (a[i] < res) ans = max(ans, res - a[i]);
+        res = max(res, a[i]);
     }
-
-    if (all_equal) {
-        cout << "Lo nang roi" << endl;
-        return 0;
-    }
-
-    // Kiểm tra nếu mảng đã sắp xếp theo thứ tự tăng dần
-    bool is_sorted = true;
-    for (int i = 1; i < n; ++i) {
-        if (a[i] < a[i - 1]) {
-            is_sorted = false;
-            break;
-        }
-    }
-
-    if (is_sorted) {
-        cout << "Lo nang roi" << endl;
-        return 0;
-    }
-
-    // Xử lý tìm hiệu lớn nhất
-    int max_so_far = a[0];
-    int max_diff = -1;
-    for (int i = 1; i < n; ++i) {
-        if (a[i] < max_so_far) max_diff = max(max_diff, max_so_far - a[i]);
-        max_so_far = max(max_so_far, a[i]);
-    }
-    cout << max_diff << endl;
+    if(ans==-1) cout<<"Lo nang roi";
+    else cout << ans << endl;
 }
