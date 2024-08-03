73b96f2820c9e4eb0f94023217196391e11756af
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-03 14:51:50 +0700

    Update DSA_P085. BIEN DOI DAY SO.cpp

--- DSA_P085. BIEN DOI DAY SO.cpp
+++ DSA_P085. BIEN DOI DAY SO.cpp
@@ -1,57 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        long long a[n], b[n];
-        for (int i = 0; i < n; ++i) cin >> a[i] >> b[i];//Giá trị và chi phí thay thế
-        //dp[i][0]: Chi phí giữ nguyên phần tử thứ i
-        //dp[i][1]: Chi phí thay đổi phần tử thứ i
-        long long dp[n + 5][2] = {};
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        long long A[N], B[N];
+        for (int i = 0; i < N; i++) cin >> A[i] >> B[i];
+        long long dp[N][3];
+        for(int i = 0; i < 3; i++) dp[0][i] = LLONG_MAX;
         dp[0][0] = 0; 
-        dp[0][1] = b[0]; 
-        for (int i = 1; i < n; ++i) {
-            if (a[i] == a[i - 1]) {
-                dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + b[i];
-                dp[i][0] = dp[i - 1][1];
-            } else {
-                dp[i][0] = min(dp[i - 1][0], dp[i - 1][1]);
-                dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + b[i];
+        dp[0][1] = B[0]; 
+        dp[0][2] = 2 * B[0];
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = LLONG_MAX; 
+                for (int k = 0; k < 3; k++) {
+                    if (A[i] + j != A[i - 1] + k) dp[i][j] = min(dp[i][j], dp[i - 1][k] + j * B[i]);
+                }
             }
         }
-        cout << min(dp[n - 1][0], dp[n - 1][1]) << endl;
+        cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << endl;
     }
-    return 0;
 }
-/*
-3
-
-3
-
-2 4
-
-2 1
-
-3 5
-
-3
-
-2 3
-
-2 10
-
-2 6
-
-4
-
-1 7
-
-3 3
-
-2 6
-
-1000000000 2
-*/
