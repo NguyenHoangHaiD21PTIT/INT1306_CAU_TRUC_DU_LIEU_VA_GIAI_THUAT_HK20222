6c8853e765fcec79ad1eb0a2cffa009e1c41429b
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-08 08:14:59 +0700

    Update DSA_P138. GIA TRI NHO NHAT.cpp

--- KIỂM TRA, KHẢO SÁT D22/DSA_P138. GIA TRI NHO NHAT.cpp
+++ KIỂM TRA, KHẢO SÁT D22/DSA_P138. GIA TRI NHO NHAT.cpp
@@ -17,3 +17,15 @@ int main(){
     }
     cout<<ans<<endl;
 }
+/*
+Giả sử A = 1 2 3 4 5 6 7 8 9 10 11 12
+n = 12, k = 3
+Để tổng đề cho có GTNN thì ta xếp như sau
+index  1   2    3    4    5    6    7    8    9    10    11    12
+G.trị  1   5    9    2    6    10   3    7    11   4     8     12
+
+12/3 thì ra 4 cụm
+gán: a1 = 1, a4 = 2, a7 = 3, a10 = 4. pos đây chinh là điểm cuối của mỗi cụm
+a2 = 5, a5 = 6, a8 = 7, a11 = 8. Hết nhóm 2
+a3 = 9, a6 = 10, a9 = 11, a12 = 12
+*/
