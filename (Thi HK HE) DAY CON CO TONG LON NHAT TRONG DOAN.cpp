737b92dbefeb8a65d5369792b6b556e1f5e9b7a8
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-08 09:31:30 +0700

    Update (Thi HK HE) DAY CON CO TONG LON NHAT TRONG DOAN.cpp

--- (Thi HK HE) DAY CON CO TONG LON NHAT TRONG DOAN.cpp
+++ (Thi HK HE) DAY CON CO TONG LON NHAT TRONG DOAN.cpp
@@ -1,37 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n, u, v;
-vector<int> a(100005,0);
-int maxSum(){
+
+int main(){
+    int n, u, v;
+    cin>>n>>u>>v;
+    vector<int>a(n);
+    for(int i = 0;i<n;i++) cin>>a[i];
     vector<int> p(n + 1, 0);
-    for (int i = 0; i < n; ++i) {
-        p[i + 1] = p[i] + a[i];
-    }
+    for (int i = 0; i < n; ++i) p[i + 1] = p[i] + a[i];
     deque<int> dq;
     int mx = INT_MIN;
     for (int i = u; i <= n; ++i) {
-        if (!dq.empty() && dq.front() < i - v) {
-            dq.pop_front();
-        }
-        if (!dq.empty()) {
-            mx = max(mx, p[i] - p[dq.front()]);
-        }
-        while (!dq.empty() && p[dq.back()] >= p[i - u + 1]) {
-            dq.pop_back();
-        }
+        if (!dq.empty() && dq.front() < i - v) dq.pop_front();
+        if (!dq.empty()) mx = max(mx, p[i] - p[dq.front()]);
+        while (!dq.empty() && p[dq.back()] >= p[i - u + 1]) dq.pop_back();
         dq.push_back(i - u + 1);
     }
-    return mx;
-}
-
-int main() {
-
-    cin >> n >> u >> v;
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-
-    cout << maxSum() << "\n";
-
-    return 0;
+    cout<<mx;
 }
