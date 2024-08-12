9de1fa3fe560b71113553411cea06ba2760c8fd4
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-12 20:46:28 +0700

    Add files via upload

---
+++ THI KY HE 2024/BIEN BAO.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string N;
+    int K;
+    cin >> N >> K;
+    stack<char> s;
+    //Với mỗi chữ số ta đang xét, ta sẽ xóa tất cả những chữ số bên tay trái nó mà nhỏ hơn nó
+    for (char c : N) {
+        while (!s.empty() && s.top() < c && K > 0) {
+            s.pop();
+            K--;
+        }
+        s.push(c);
+    }
+	//Trong trường hợp chạy đến hết xâu rồi mà vẫn chưa hết số lần cắt 
+	//Lúc này, đỉnh stack lưu số bé nhất, tăng dần cho đến số cuối
+    while (K > 0) {
+        s.pop();
+        K--;
+    }
+
+    string ans = "";
+    while (!s.empty()) {
+        ans += s.top();
+        s.pop();
+    }
+    reverse(ans.begin(), ans.end());
+
+    cout << ans << endl;
+    return 0;
+}
---
+++ THI KY HE 2024/XOA CANH.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int par[100005], sze[100005], cnt[100005];//cnt[i]: cạnh thứ i bị liêt kê i lần trong tập cạnh xoá
+int find(int x){
+	if(x==par[x]) return x;
+	return par[x] = find(par[x]);
+}
+
+bool Union(int x, int y){
+	x = find(x);
+	y = find(y);
+	if(x==y) return false;
+	if(sze[x]<sze[y]) swap(x,y);
+	par[y] = x;
+	sze[x]+=sze[y];
+	return true;	
+}
+
+int main(){
+    int n, m, q;
+    cin>>n>>m>>q;
+    for(int i=1;i<=n;i++){
+		par[i] = i;
+		sze[i] = 1;
+	}
+	
+	//Danh sách cạnh
+	vector<pair<int, int>>edge(m + 2);
+	vector<bool>deleted(m + 2, false);
+	for(int i = 1;i<=m;i++) cin >> edge[i].first >> edge[i].second;
+	
+	//Danh sách truy vấn {STT cạnh, đỉnh}
+	vector<pair<int, int>>truyVan(q);
+	for(int i = 0;i<q;i++){
+	    cin >> truyVan[i].first >> truyVan[i].second;
+	    deleted[truyVan[i].first] = true;
+	}
+	//Đếm số lần cạnh bị xoá bị liệt kê
+	for(int i = 0;i<q;i++) cnt[truyVan[i].first]++;//đếm số lần cạnh bị xoá xuất hiện trong tập truy vấn
+	
+	//Thêm các cạnh không bị xoá
+	for(int i = 1;i<=m;i++){
+	    if(deleted[i]==false) Union(edge[i].first, edge[i].second);
+	}
+	
+	vector<int>ans;
+	for(int i = q - 1;i>=0;i--){
+	    int stt = truyVan[i].first; //STT cạnh cần xoá
+	    int x = truyVan[i].second; //Đỉnh check số phần tử cùng TPLT
+	    ans.push_back(sze[find(x)]);
+	    int a = edge[stt].first, b = edge[stt].second;
+	    if(cnt[stt]==1) Union(a, b);
+	    cnt[stt]--;
+	}
+	
+	reverse(ans.begin(), ans.end());
+	for(int x: ans) cout<<x<<endl;
+}
