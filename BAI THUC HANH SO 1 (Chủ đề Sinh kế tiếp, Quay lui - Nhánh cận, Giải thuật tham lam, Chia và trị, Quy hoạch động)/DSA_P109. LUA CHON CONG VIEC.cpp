41a9fdf8d11a3d9b7b3229fd9e56b81cb6e636f2
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-18 00:15:54 +0700

    Update DSA_P109. LUA CHON CONG VIEC.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/DSA_P109. LUA CHON CONG VIEC.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/DSA_P109. LUA CHON CONG VIEC.cpp
@@ -1,34 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
-struct job {
-	int dl, pr;
+struct task{
+	long long deadline, profit; 
 };
 
-bool cmp(job a, job b){
-	if (a.dl != b.dl) return a.dl > b.dl;
-	else return a.pr > b.pr;
+bool cmp (task a, task b){
+	if(a.profit!=b.profit) return a.profit>b.profit;
+	return a.deadline > b.deadline;
 }
+task a[100007]; 
 
-int main() {
-	int n;
-	cin >> n;
-	job a[n];
-	for (int i = 0; i < n; i++) cin >> a[i].dl >> a[i].pr;
-	sort(a, a + n, cmp);
-	priority_queue<ll>q;
-	ll sum = 0, tmp = 0;
-	for (int i = n; i >= 1; i--) {
-		while (tmp < n && a[tmp].dl >= i) {
-			q.push(a[tmp].pr);
-			tmp++;
-		}
-		if (!q.empty()) {
-			sum += q.top();
-			q.pop();
-		}
+int main(){
+	long long n;
+	cin>>n;
+	for(long long i=0;i<n;i++) cin>>a[i].deadline>>a[i].profit;
+	sort(a,a+n,cmp);
+	long long ans = 0;
+	set<int>tGian;//Trong này chỉ chứa các đơn vị thời gian chưa sử dụng
+	for(int i = 1;i<=n;i++) tGian.insert(i);
+	for(int i = 0;i<n;i++){
+	    auto x = tGian.upper_bound(a[i].deadline);
+	    if(x==tGian.begin()) continue;
+	    /*LÍ LUẬN: 
+	    Ví dụ: dl là 3. Các đơn vị thời gian chưa dùng là 5 7 8 15 thì bỏ. Tức là min(tg chưa dùng) > dl --> Chả có chỗ xếp
+	    Ta tìm được vị trí đầu tiên > dl --> Lùi để về vị trí sau cùng gần dl nhất 
+	    Xếp sau để còn chỗ cho các việc có dl sớm*/
+	    
+	    //XẾP VIỆC
+	    x--;//Xếp vào Vị trí sát dl nhất
+	    ans+=a[i].profit;//Làm xong trước dl thì có lãi
+	    tGian.erase(x); //thời gian x đã có việc xếp vào
 	}
-	cout << sum << endl;
+	cout<<ans;
 }
