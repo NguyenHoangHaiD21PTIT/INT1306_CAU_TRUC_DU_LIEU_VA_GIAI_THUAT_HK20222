8e01b6264c2731b9e0042aac4bb2a99e2b0abfb6
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-17 22:04:05 +0700

    Update DSA_P054. HINH CHU NHAT DON SAC.cpp

--- BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P054. HINH CHU NHAT DON SAC.cpp
+++ BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSA_P054. HINH CHU NHAT DON SAC.cpp
@@ -5,7 +5,6 @@ long long kq(long long a[], long long n){
     long long l[n], r[n];
     // Tìm Vị trí phần tử đầu tiên bên trái, bên phải a[i] bé hơn a[i]
     //l, r là cận trái, phải của HCN
-    for(long long i = 0;i<n;i++) cin>>a[i];
     stack<long long>st1, st2;
     for(long long i = n - 1;i>=0;i--){
         while(!st1.empty() && a[i] <= a[st1.top()]) st1.pop();
