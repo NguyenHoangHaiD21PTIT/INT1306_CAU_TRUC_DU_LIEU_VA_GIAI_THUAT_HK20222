64b8f70c46152dd15410bbc34718cf31bfb56133
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-17 18:24:30 +0700

    Update DSA_P036. CHIA DOI.cpp

--- ÔN TẬP ĐỒ THỊ/DSA_P036. CHIA DOI.cpp
+++ ÔN TẬP ĐỒ THỊ/DSA_P036. CHIA DOI.cpp
@@ -5,9 +5,11 @@ long long f[35][35];
 void prepare(){
     for(int i=0;i<=30;i++){
         for(int j=0;j<=30;j++){
-            if(i==0)f[i][j]=1;
-            if(j==0)f[i][j]=f[i-1][1];
+            if(i==0)f[i][j]=1; //Không có thanh nguyên nào --> Cách duy nhất là ăn lần lượt từng j nửa thanh 
+            if(j==0)f[i][j]=f[i-1][1]; //Không có nửa thanh nào --> Cách duy nhất là lấy 1 thanh nguyên, bẻ đôi, ăn 1 nửa để 1 nửa --> Bớt đi 1 thanh nguyên và thêm nửa thanh
             if(i&&j)f[i][j]=f[i-1][j+1]+f[i][j-1];
+            //Cách 1: Lấy 1 thanh nguyên, bẻ đôi, ăn 1 nửa để 1 nửa --> Bớt đi 1 thanh nguyên và thêm nửa thanh
+            //Cách 2: Ăn luôn 1 nửa thanh sẵn có
         }
     }
 }
