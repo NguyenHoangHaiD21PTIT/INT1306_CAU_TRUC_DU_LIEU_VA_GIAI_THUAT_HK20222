d7df19bb09881505eb9cbdd914ce686f8decd8ad
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-17 18:09:29 +0700

    Update DSA_P111. DEM KI TU B.cpp

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P111. DEM KI TU B.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P111. DEM KI TU B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 long long f[50], cnt[50];
 
-void gen(){
+void gen(){ //cnt[i]: Số ký tự B trong xâu thứ i, f[i]: Độ dài xâu thứ i
 	f[0]=1;
 	f[1]=1;
 	cnt[0]=0;
@@ -17,11 +17,8 @@ void gen(){
 long long count(long long n, long long k){
 	if(n==0) return 0;
 	if(n==1) return 1;
-	if(k<=f[n-1]){
-		return count(n-1,k);
-	} else {
-		return cnt[n-1] + count(n-2,k-f[n-1]);
-	}
+	if(k<=f[n-1]) return count(n-1,k);
+	else return cnt[n-1] + count(n-2,k-f[n-1]);
 }
 
 int main(){
