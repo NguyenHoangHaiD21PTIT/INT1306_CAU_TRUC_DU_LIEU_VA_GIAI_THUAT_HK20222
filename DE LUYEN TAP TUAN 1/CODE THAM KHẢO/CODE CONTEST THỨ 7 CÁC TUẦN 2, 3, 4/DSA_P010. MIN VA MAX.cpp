c1fc3c1b2d094d37fd814111cc338d5d16c2b49d
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-17 18:16:31 +0700

    Update DSA_P010. MIN VA MAX.cpp

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P010. MIN VA MAX.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P010. MIN VA MAX.cpp
@@ -23,3 +23,7 @@ int main() {
     for (int i = 0; i < n; ++i) res = max(res, (long long)a[i] * (r[i] - l[i] + 1));
     cout << res;
 }
+/*Hiểu là: Đề bài yêu cầu: Xét mọi dãy con liên tiếp, mỗi dãy con lấy phần tử nhỏ nhất nhân với chiều dài dãy con đó
+Mà mỗi phần tử đều là min của dãy con liên tiếp nào đó --> Vét cạn kiểu mới: Mỗi phần tử, ta sẽ xem nó là min của những dãy con liên tiếp nào, rồi lấy phần tử đó * chiều dài dãy con đó
+Đề bài yêu cầu: Lấy max. Vậy thì mỗi phần tử, ta chỉ cần xem nó là min của dãy con liên tiếp dài nhất nào. Rồi lấy max những phép tính đó.
+*/
