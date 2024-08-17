d09af51dc6f6ce8904eabf9374aca2fcaff91557
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-18 00:19:50 +0700

    Update DSA_P083. DAY CON NHO NHAT CO UCLN BANG K (CAN TEST 80%).cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P083. DAY CON NHO NHAT CO UCLN BANG K (CAN TEST 80%).cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P083. DAY CON NHO NHAT CO UCLN BANG K (CAN TEST 80%).cpp
@@ -1,61 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define int long long
+#define ii pair<int, int>
+const int N = 2e5+5, M = 1e3+5, MOD = 1e9+9, inf = 1e18;
+int n, m, x, y, res, test, sum1, k, d;
+int a[N], t[N][18], vt[N], l[N], r[N];
 
-long long solve(long long a[], long long l, long long r, long long k) {
-    long long ans = LLONG_MAX;
-    // Giới hạn chiều dài đoạn con tối đa là 10
-    long long len = r - l + 1;
-    if(len>10000) len = 10;
-    for (int i = l; i <= l + len; i++) {
-        long long res = a[i];
-        for (int j = i + 1; j < i + len && j <= r; j++) {
-            res = __gcd(res, a[j]);
-            if (res == k) {
-                ans = min(ans, (long long)(j - i + 1));
-                break;
-            }
-        }
-    }
-    return ans;
+int get(int l, int r) {
+    int k = __lg(r - l + 1);
+    return __gcd(t[l][k], t[r - (1 << k) + 1][k]);
 }
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        long long n, k;
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> test;
+    while (test--) {
         cin >> n >> k;
-        long long a[100005];
-        for (long long i = 1; i <= n; i++) cin >> a[i];
-        vector<pair<long long, long long>> range; // Chỉ quét UCLN trong các khoảng [l; r] có trong vector này
-        long long ans = LLONG_MAX;
-        long long l = -1, r = -1;
-        for (long long i = 1; i <= n; i++) {
-            if (a[i] == k) {
-                ans = 1; // Tìm thấy phần tử bằng k
-                break;
-            } else if (a[i] % k == 0) {
-                if (l == -1) l = r = i; // Nếu chưa có đoạn thì bắt đầu từ đây
-                else r = i; // Nếu có đoạn rồi thì mở rộng nó ra
-            } else {
-                if (r != -1) {
-                    range.push_back({l, r}); // Đoạn bị gãy thì ghi nhận đoạn trước đó vào
-                    l = r = -1; // Reset các chỉ số
+        res = n; y = 0;
+        for (int i = 1; i <= n; ++i) {
+            cin >> a[i];
+            t[i][0] = a[i];
+        }
+        int mask = __lg(n);
+        for (int j = 1; j <= mask; ++j)
+            for (int i = 1; i + (1 << j) - 1 <= n; ++i)
+                t[i][j] = __gcd(t[i][j - 1], t[i + (1 << (j - 1))][j - 1]);
+        for (int i = 1; i <= n; ++i) if (a[i] % k == 0) {
+            int d = 1, c = min(res, n - i + 1);
+            while (d <= c) {
+                int mid = (d + c) >> 1;
+                x = get(i, i + mid - 1);
+                if (x == k) {
+                    ++y;
+                    res = min(res, mid);
+                    c = mid - 1;
                 }
+                else if (x < k) c = mid - 1;
+                else d = mid + 1;
             }
         }
-
-        // Thêm đoạn cuối cùng nếu có
-        if (r != -1) range.push_back({l, r});
-
-        if (ans != 1) {
-            // Xử lý từng khoảng trong danh sách
-            for (auto p : range) {
-                long long l = p.first, r = p.second;
-                ans = min(ans, solve(a, l, r, k));
-            }
-        }
-        cout << (ans == LLONG_MAX ? -1 : ans) << endl;
+        cout << (y ? res : -1) << endl;
     }
-    return 0;
 }
