71a3ae59f88ce7fd58f5a0ece5c2293b2a4dcd32
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-18 12:22:04 +0700

    Update B. UOC CHUNG LON NHAT.cpp

--- CODE ICPC TUAN 5/B. UOC CHUNG LON NHAT.cpp
+++ CODE ICPC TUAN 5/B. UOC CHUNG LON NHAT.cpp
@@ -12,3 +12,7 @@ int main() {
     }
     cout << res;
 }
+/*Gọi d là UCLN của dãy số --> a1+a2+ ...+ an = d*b1 + d*b2 + ... + d*bn = M ->d(b1 + b2+... + bn) = M
+Như vậy, thì d là ước của M. Mặt khác, b1, b2, ..., bn tất cả đều >=1. Do đó nên tổng này tối thiểu phải >=N.
+Khi chọn d là UCLN thì tổng này là M/d. Vậy, M/d>=n--> d*n<=m
+*/
