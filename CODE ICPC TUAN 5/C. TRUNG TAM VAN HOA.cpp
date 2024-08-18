013361aeea73f01fe568cc4a83448ee090b1b8cc
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-18 12:57:30 +0700

    Update C. TRUNG TAM VAN HOA.cpp

--- CODE ICPC TUAN 5/C. TRUNG TAM VAN HOA.cpp
+++ CODE ICPC TUAN 5/C. TRUNG TAM VAN HOA.cpp
@@ -19,3 +19,7 @@ int main(){
     }
     cout << fixed << setprecision(2) << ans;
 }
+/*
+Giả sử y = (x - a)^2 + (x - b)^2 + (x - c)^2 đạt min
+y' = 2(x - a) + 2(x - b) + 2(x - c) = 2(3*x - (a + b + c)). y' có nghiệm đơn và đổi dấu qua nghiệm đơn --> y'=0 khi 3*x = (a + b + c)
+*/
