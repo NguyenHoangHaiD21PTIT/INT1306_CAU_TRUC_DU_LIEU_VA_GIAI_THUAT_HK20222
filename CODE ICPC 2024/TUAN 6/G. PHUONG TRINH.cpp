e94f37e1cfc6f2d0801889a78579a78ebffcd820
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-04 19:15:45 +0700

    Update G. PHUONG TRINH.cpp

--- CODE ICPC 2024/TUAN 6/G. PHUONG TRINH.cpp
+++ CODE ICPC 2024/TUAN 6/G. PHUONG TRINH.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+double myRound(double a) {
+    return round(a * 1e6) / 1e6;
+}
 int main() {
     int t;
     cin >> t;
@@ -12,18 +15,22 @@ int main() {
                 if (c != 0) continue; 
                 else {
                     cout << -1;
-                    return 0; //Vô số nghiệm
+                    return 0; // Vô số nghiệm
                 }
-            } else  s.insert(-c / b); 
+            } else {
+                s.insert(myRound(-c / b)); 
+            }
         } else {
             double delta = b * b - 4 * a * c;
             if (delta < 0) continue; 
-            else if (delta == 0) s.insert(-b / (2 * a));  
-            else {
-                s.insert((-b + sqrt(delta)) / (2 * a)); 
-                s.insert((-b - sqrt(delta)) / (2 * a)); 
+            else if (delta == 0) {
+                s.insert(myRound(-b / (2 * a)));  
+            } else {
+                s.insert(myRound((-b + sqrt(delta)) / (2 * a))); 
+                s.insert(myRound((-b - sqrt(delta)) / (2 * a))); 
             }
         }
     }
     cout << s.size(); 
 }
+
