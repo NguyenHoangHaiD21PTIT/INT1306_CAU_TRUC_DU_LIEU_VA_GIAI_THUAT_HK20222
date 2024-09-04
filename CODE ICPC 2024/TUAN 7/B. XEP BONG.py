31ca93eeef73bd0701709ac12fc40dd5bc8212ce
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-04 15:52:44 +0700

    Update B. XEP BONG.py

--- CODE ICPC 2024/TUAN 7/B. XEP BONG.py
+++ CODE ICPC 2024/TUAN 7/B. XEP BONG.py
@@ -2,7 +2,7 @@ import math
 B = [0] * 1001
 B[0] = 1
 def Bell():
-    for i in range(1, 50):
+    for i in range(1, 51):
         B[i] = 0
         for j in range(i): B[i] += math.comb(i - 1, j) * B[j]
 Bell()
