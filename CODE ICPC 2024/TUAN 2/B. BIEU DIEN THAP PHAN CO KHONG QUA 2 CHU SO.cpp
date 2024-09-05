5f5ce417cd6516a109917c3af7b93b884db3c8af
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-05 08:09:49 +0700

    Update B. BIEU DIEN THAP PHAN CO KHONG QUA 2 CHU SO.cpp

--- CODE ICPC 2024/TUAN 2/B. BIEU DIEN THAP PHAN CO KHONG QUA 2 CHU SO.cpp
+++ CODE ICPC 2024/TUAN 2/B. BIEU DIEN THAP PHAN CO KHONG QUA 2 CHU SO.cpp
@@ -1,22 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string s;
-    cin>>s;
-    int n = stoi(s), k = s.size(), ans = 0;
-    set<int>res;
-    for(int x1 = 0;x1<=9;x1++){
-        for(int x2 = 0;x2<=9;x2++){
-            for(int mask = 0;mask<pow(2, k);mask++){
+string s;
+int k, a[100], ok; // a[i] = 0 là tại đó, ta lấy con số x1, a[i] = 1 là con số x2
+
+void kt() {
+    for(int i = 0; i < k; i++) a[i] = 0;
+}
+
+void sinh() {
+    int i = k - 1;
+    while (i >= 0 && a[i] == 1) {
+        a[i] = 0;
+        i--;
+    }
+    if(i == -1) ok = 0;
+    else a[i] = 1;
+}
+
+int main() {
+    cin >> s;
+    int n = stoi(s);
+    set<int> res;
+    for(int x1 = 0; x1 <= 9; x1++) {
+        for(int x2 = 0; x2 <= 9; x2++){
+            for(k = 1;k<=s.size();k++){ //Sinh nhị phân (x1, x2) độ dài cấu hình tăng dần từ 1 đến k
                 long long num = 0;
-                for(int i = 0;i<k;i++){
-                    if(mask&(1<<i)) num = num * 10 + x1;
-                    else num = num * 10 + x2;
-                    if(num<=n) res.insert(num);
+                kt();
+                ok = 1;
+                for(int i = 0;i<k;i++) num = num * 10 + x1;
+                while(ok) {
+                    num = 0; 
+                    for(int i = 0; i < k; i++) {
+                        if(a[i] == 0) num = num * 10 + x1;
+                        else num = num * 10 + x2;
+                    }
+                    if(num >= 1 && num <= n) res.insert(num);
+                    sinh();
                 }
             }
         }
     }
-    cout<<res.size() - 1;
+    cout << res.size() << endl;
 }
