be09f32a3ab92eb4d33f25d9de45b0bcf751f54b
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-05 08:32:47 +0700

    Update 1D. THANG MAY.cpp

--- CODE ICPC 2024/TUAN 1/1D. THANG MAY.cpp
+++ CODE ICPC 2024/TUAN 1/1D. THANG MAY.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, st, en, u, d;
-int visited[1000006];
-
+vector<bool> visited; // Khởi tạo vector bool
 int BFS() {
     if (st == en) return 0;
-    queue<pair<int, int>>q;
+    queue<pair<int, int>> q;
     q.push({st, 0});
     visited[st] = true;
     while (!q.empty()) {
         int curFl = q.front().first;
         int cnt = q.front().second;
         q.pop();
-        if(curFl==en) return cnt;
+        if (curFl == en) return cnt;
         int nextFl = curFl + u;
         if (nextFl <= n && !visited[nextFl]) {
             q.push({nextFl, cnt + 1});
-            visited[nextFl];
+            visited[nextFl] = true; 
         }
         nextFl = curFl - d;
-        if (nextFl >= 1 && !visited[nextFl]) {
+        if (nextFl >= 1 && !visited[nextFl]) { 
             q.push({nextFl, cnt + 1});
-            visited[nextFl];
+            visited[nextFl] = true; 
         }
     }
     return -1;
 }
-
 int main() {
-    cin>>n>>st>>en>>u>>d;
-    cout<<BFS();
+    cin >> n >> st >> en >> u >> d;
+    visited.resize(n + 1, false); // Khởi tạo vector với kích thước n+1 và tất cả giá trị là false
+    cout << BFS();
+    return 0;
 }
