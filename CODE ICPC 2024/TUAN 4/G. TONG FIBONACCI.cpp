1ffd9c524b9ae003bd0880d6877873fb3c3fa55d
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-05 07:31:45 +0700

    Update G. TONG FIBONACCI.cpp

--- CODE ICPC 2024/TUAN 4/G. TONG FIBONACCI.cpp
+++ CODE ICPC 2024/TUAN 4/G. TONG FIBONACCI.cpp
@@ -1,142 +1,101 @@
 #include <bits/stdc++.h>
-//Segment tree + lazy update + mul matrix
-
-#define forn(i, n) for(int i = 0 ; (i) < (n) ; ++i)
-#define eprintf(...) fprintf(stderr, __VA_ARGS__),fflush(stderr)
-#define sz(a) ((int)(a).size())
-#define all(a) (a).begin(),a.end()
-#define pw(x) (1LL<<(x))
-
 using namespace std;
-
 typedef long long ll;
-typedef double dbl;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-
-const int inf = 1.01e9;
-const dbl eps = 1e-9;
-
-
-const int mod = 1e9 + 7;
-
-struct mat
-{
-    int a[2][2];
-    mat()
-    {
-        memset(a, 0, sizeof a);
-    }
-    mat(int x)
-    {
-        memset(a, 0, sizeof a);
-        a[0][0] = a[1][1] = x;
+const int MOD = 1e9 + 7;
+struct Matrix {
+    int matrix[2][2];
+    Matrix() {
+        memset(matrix, 0, sizeof matrix);
+    } 
+    Matrix(int x) {
+        memset(matrix, 0, sizeof matrix);
+        matrix[0][0] = matrix[1][1] = x;
     }
 };
-
-inline mat operator * (mat A, mat B)
-{
-    mat C;
-    forn(i, 2) forn(j, 2) forn(k, 2) C.a[i][j] = (C.a[i][j] + A.a[i][k] * (ll)B.a[k][j]) % mod;
-    return C;
-}
-inline mat operator + (mat A, mat B)
-{
-    mat C;
-    forn(i, 2) forn(j, 2) C.a[i][j] = (A.a[i][j] + B.a[i][j]) % mod;
-    return C;
-}
-
-inline mat mpow(mat A, ll b)
-{
-    mat R(1);
-    while (b)
-    {
-        if (b & 1) R = R * A;
-        A = A * A;
-        b >>= 1;
+Matrix multiply(Matrix A, Matrix B) {
+    Matrix result;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) result.matrix[i][j] = (result.matrix[i][j] + A.matrix[i][k] * (ll)B.matrix[k][j]) % MOD;
+        }
     }
-    return R;
+    return result;
 }
-
-inline mat fib(ll b)
-{
-    mat A;
-    A.a[0][0] = 1;
-    A.a[0][1] = 1;
-    A.a[1][0] = 1;
-    return mpow(A, b);
+Matrix add(Matrix A, Matrix B) {
+    Matrix result;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) result.matrix[i][j] = (A.matrix[i][j] + B.matrix[i][j]) % MOD;
+    }
+    return result;
 }
-
-const int N = 1 << 17;
-
-mat t[N * 2];
-mat s[N * 2];
-
-void push(int x)
-{
-    t[2 * x] = t[2 * x] * t[x];
-    t[2 * x + 1] = t[2 * x + 1] * t[x];
-    s[2 * x] = s[2 * x] * t[x];
-    s[2 * x + 1] = s[2 * x + 1] * t[x];
-    t[x] = mat(1);
+Matrix power(Matrix A, ll exponent) {
+    Matrix result(1);
+    while (exponent) {
+        if (exponent & 1) result = multiply(result, A);
+        A = multiply(A, A);
+        exponent >>= 1;
+    }
+    return result;
 }
-
-void upd(int x, int l, int r, int lq, int rq, mat val)
-{
-    if (lq <= l && r <= rq)
-    {
-        t[x] = t[x] * val;
-        s[x] = s[x] * val;
+Matrix fibonacciMatrix(ll exponent) {
+    Matrix fibMatrix;
+    fibMatrix.matrix[0][0] = 1;
+    fibMatrix.matrix[0][1] = 1;
+    fibMatrix.matrix[1][0] = 1;
+    return power(fibMatrix, exponent);
+}
+const int MAX_N = 1 << 17;
+Matrix segmentTree[MAX_N * 2];
+Matrix lazyUpdate[MAX_N * 2];
+void applyLazy(int nodeIndex) {
+    segmentTree[2 * nodeIndex] = multiply(segmentTree[2 * nodeIndex], lazyUpdate[nodeIndex]);
+    segmentTree[2 * nodeIndex + 1] = multiply(segmentTree[2 * nodeIndex + 1], lazyUpdate[nodeIndex]);
+    lazyUpdate[2 * nodeIndex] = multiply(lazyUpdate[2 * nodeIndex], lazyUpdate[nodeIndex]);
+    lazyUpdate[2 * nodeIndex + 1] = multiply(lazyUpdate[2 * nodeIndex + 1], lazyUpdate[nodeIndex]);
+    lazyUpdate[nodeIndex] = Matrix(1);
+}
+void updateRange(int nodeIndex, int leftBound, int rightBound, int queryLeft, int queryRight, Matrix value) {
+    if (queryLeft <= leftBound && rightBound <= queryRight) {
+        lazyUpdate[nodeIndex] = multiply(lazyUpdate[nodeIndex], value);
+        segmentTree[nodeIndex] = multiply(segmentTree[nodeIndex], value);
         return;
     }
-    if (l >= rq || lq >= r) return;
-    push(x);
-    int m = (l + r) >> 1;
-    upd(2 * x, l, m, lq, rq, val);
-    upd(2 * x + 1, m, r, lq, rq, val);
-    s[x] = s[2 * x] + s[2 * x + 1];
+    if (leftBound >= queryRight || queryLeft >= rightBound) return;
+    applyLazy(nodeIndex);
+    int middle = (leftBound + rightBound) / 2;
+    updateRange(2 * nodeIndex, leftBound, middle, queryLeft, queryRight, value);
+    updateRange(2 * nodeIndex + 1, middle, rightBound, queryLeft, queryRight, value);
+    segmentTree[nodeIndex] = add(segmentTree[2 * nodeIndex], segmentTree[2 * nodeIndex + 1]);
 }
-
-mat sum(int x, int l, int r, int lq, int rq)
-{
-    if (lq <= l && r <= rq) return s[x];
-    if (l >= rq || lq >= r) return mat();
-    push(x);
-    int m = (l + r) >> 1;
-    return sum(2 * x, l, m, lq, rq) + sum(2 * x + 1, m, r, lq, rq);
+Matrix querySum(int nodeIndex, int leftBound, int rightBound, int queryLeft, int queryRight) {
+    if (queryLeft <= leftBound && rightBound <= queryRight) return segmentTree[nodeIndex];
+    if (leftBound >= queryRight || queryLeft >= rightBound) return Matrix();
+    applyLazy(nodeIndex);
+    int middle = (leftBound + rightBound) / 2;
+    return add(querySum(2 * nodeIndex, leftBound, middle, queryLeft, queryRight), querySum(2 * nodeIndex + 1, middle, rightBound, queryLeft, queryRight));
 }
-
-int main()
-{
-    int n, m;
-    cin>>n>>m;
-    forn(i, 2 * N) t[i] = mat(1);
-    forn(i, n)
-    {
-        int x;
-        scanf("%d", &x);
-        s[i + N] = fib(x);
+int main() {
+    int arraySize, queryCount;
+    cin >> arraySize >> queryCount;
+    for (int i = 0; i < 2 * MAX_N; i++) lazyUpdate[i] = Matrix(1);
+    for (int i = 0; i < arraySize; i++) {
+        int value;
+        scanf("%d", &value);
+        segmentTree[i + MAX_N] = fibonacciMatrix(value);
     }
-    for (int i = N - 1; i >= 0; --i) s[i] = s[2 * i] + s[2 * i + 1];
-    forn(i, m)
-    {
-        int tp;
-        scanf("%d", &tp);
-        if (tp == 1)
-        {
-            int l, r, x;
-            scanf("%d%d%d", &l, &r, &x);
-            upd(1, 0, N, l - 1, r, fib(x));
-        }
-        else
-        {
-            int l, r;
-            scanf("%d%d", &l, &r);
-            mat R = sum(1, 0, N, l - 1, r);
-            printf("%d\n", R.a[0][1]);
+    for (int i = MAX_N - 1; i >= 0; i--) segmentTree[i] = add(segmentTree[2 * i], segmentTree[2 * i + 1]);
+    for (int i = 0; i < queryCount; i++) {
+        int queryType;
+        scanf("%d", &queryType);
+        if (queryType == 1) {
+            int left, right, x;
+            scanf("%d%d%d", &left, &right, &x);
+            updateRange(1, 0, MAX_N, left - 1, right, fibonacciMatrix(x));
+        } else {
+            int left, right;
+            scanf("%d%d", &left, &right);
+            Matrix result = querySum(1, 0, MAX_N, left - 1, right);
+            printf("%d\n", result.matrix[0][1]);
         }
     }
-
-    return 0;
 }
