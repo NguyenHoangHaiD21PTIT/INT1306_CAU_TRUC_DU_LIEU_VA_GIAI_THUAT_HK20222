97ea868fa116bd0134c56e7c97506b2886b3e2fb
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-17 09:09:47 +0700

    Update G. DAY SO VOI CAC PHAN TU LIEN TIEP.cpp

--- CODE ICPC 2024/VONG LOAI ICPC/G. DAY SO VOI CAC PHAN TU LIEN TIEP.cpp
+++ CODE ICPC 2024/VONG LOAI ICPC/G. DAY SO VOI CAC PHAN TU LIEN TIEP.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 using ll = long long;
-const int mod = 1e9 + 7;
-
 int main(){
     ll m;
     cin >> m;
@@ -16,6 +13,5 @@ int main(){
             }
         }
     cout<<res;
-
 }
 
