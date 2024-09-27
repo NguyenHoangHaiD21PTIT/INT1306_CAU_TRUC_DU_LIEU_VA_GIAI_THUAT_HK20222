163555538cb1b5c9d7765dfa3303f7542da4cfc0
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-09-28 00:05:49 +0700

    Update D. DUONG TRON.cpp

--- CODE ICPC 2024/CODE CHUNG KET/D. DUONG TRON.cpp
+++ CODE ICPC 2024/CODE CHUNG KET/D. DUONG TRON.cpp
@@ -1,50 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int MOD = 1e9 + 7, MAX = 100050;  
-long long fac[MAX], ivFac[MAX];  
-
-long long powMod(long long a, long long b) {
-    long long res = 1;
-    while (b > 0) {
-        if (b % 2 == 1) res = res * a % MOD;
-        a = a * a % MOD;
-        b /= 2;
-    }
-    return res;
-}
-
-void pre() {
-    fac[0] = 1;
-    for (long long i = 1; i < MAX; ++i) fac[i] = fac[i - 1] * i % MOD;
-    ivFac[MAX - 1] = powMod(fac[MAX - 1], MOD - 2);  
-    for (long long i = MAX - 2; i >= 0; --i) ivFac[i] = ivFac[i + 1] * (i + 1) % MOD;
-}
-
-long long C(long long n, long long k) {
-    if (k < 0 || k > n) return 0;
-    return fac[n] * ivFac[k] % MOD * ivFac[n - k] % MOD;
-}
-
+const long double epsilon = 1e-9;
 int main() {
-    long long N, M;
-    cin >> N >> M;
-    pre();
-    vector<pair<long long, long long>> f;
-    for (int i = 2; i * i <= M; ++i) {
-        long long cnt = 0;
-        while (M % i == 0) {
-            M /= i;
-            cnt++;
+    long double X, Y, r;
+    cin >> X >> Y >> r;
+    long long count = 0;
+    long long xMin = X - r, xMax = X + r;
+    for (long long x = xMin; x <= xMax; ++x) {
+        long double distX = fabs(x - X);
+        if ((distX - r) <= epsilon) {
+            long double distY = sqrt(r * r - distX * distX);
+            long long yMin = Y - distY, yMax = Y + distY;
+            while (yMin <= Y + distY && (x - X) * (x - X) + (yMin - Y) * (yMin - Y) - r * r > epsilon) ++yMin;
+            while (yMax >= Y - distY && (x - X) * (x - X) + (yMax - Y) * (yMax - Y) - r * r > epsilon) --yMax;
+            count += (yMax - yMin + 1);
         }
-        if (cnt > 0) f.push_back({i, cnt});
     }
-    if (M > 1) f.push_back({M, 1});
-    long long res = 1;
-    for (auto x: f) {
-        long long p = x.first, k = x.second;
-        long long comb = C(k + N - 1, k);
-        res = res * comb % MOD;
-    }
-    cout << res;
+    cout << count << endl;
 }
