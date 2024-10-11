aca4a813e0f3c50e810c58a8d1b37472a6d04efa
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-11 19:25:28 +0700

    Update DSA_P331. PHAN TICH RA THUA SO NGUYEN TO.cpp

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P331. PHAN TICH RA THUA SO NGUYEN TO.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P331. PHAN TICH RA THUA SO NGUYEN TO.cpp
@@ -1,25 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define ll long long
+vector<ll> primes;
+
+void sieve() {
+    const int MAX = 1e7;
+    vector<bool> isPrime(MAX + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (ll i = 2; i * i <= MAX; i++) {
+        if (isPrime[i]) {
+            for (ll j = i * i; j <= MAX; j += i) isPrime[j] = false;
+        }
+    }
+    for (ll i = 2; i <= MAX; i++) {
+        if (isPrime[i]) primes.push_back(i);
+    }
+}
+
+int main() {
+    sieve();
     int t;
-    cin>>t;
-    while(t--){
-        map<long long, long long>mp;
-        long long n;
-        cin>>n;
-        for(int i = 2;i<=sqrt(n);i++){
-            if(n%i==0){
-                long long dem = 0;
-                while(n%i==0){
-                    n/=i;
-                    dem++;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        for (ll p : primes) {
+            if (p * p > n) break;
+            if (n % p == 0) {
+                int count = 0;
+                while (n % p == 0) {
+                    count++;
+                    n /= p;
                 }
-                mp[i] = dem;
+                cout << p << " " << count << endl;
             }
         }
-        if(n!=1) mp[n] = 1;
-        for(auto x: mp) cout<<x.first<<" "<<x.second<<endl;
+        if (n > 1) cout << n << " " << 1 << endl; 
         cout<<endl;
     }
 }
