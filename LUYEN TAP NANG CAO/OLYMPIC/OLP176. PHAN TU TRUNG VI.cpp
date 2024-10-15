e585cfe6e0bf039d0e0d62515455c05dbc345953
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-16 00:45:14 +0700

    Update OLP176. PHAN TU TRUNG VI.cpp

--- LUYEN TAP NANG CAO/OLYMPIC/OLP176. PHAN TU TRUNG VI.cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP176. PHAN TU TRUNG VI.cpp
@@ -1,1 +1,25 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        if (a[i] < m) a[i] = -1;  
+        else if (a[i] == m) a[i] = 0; 
+        else a[i] = 1; 
+    }
+    long long cnt = 0;
+    unordered_map<long long, long long> pre[2]; // 0 cho chẵn, 1 cho lẻ
+    pre[0][0] = 1; 
+    long long sum = 0, cntM = 0; 
+    for (int i = 1; i <= n; i++) {
+        sum += a[i]; 
+        if (a[i] == 0) cntM++; 
+        if (cntM % 2 == 0)  cnt += pre[1][sum]; 
+        else              cnt += pre[0][sum]; 
+        pre[cntM % 2][sum]++; 
+    }
+    cout << cnt; 
+}
