ffb3b7269db142b84a82adc6dd8f58b8680dde90
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-19 21:33:52 +0700

    Update OLP010. TAM GIAC.cpp

--- LUYEN TAP NANG CAO/OLYMPIC/OLP010. TAM GIAC.cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP010. TAM GIAC.cpp
@@ -1,1 +1,24 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, id = 0, ans = 0;
+    cin>>n;
+    int a[n];
+    for(int i = 0;i<n;i++) cin>>a[i];
+    set<pair<int, int>>s;
+    for(int i = 0;i<n;i++){
+        if(s.size()<2) s.insert({a[i], i});
+        else{
+            while (s.size() > 1) {  
+                auto t = ++s.begin();  
+                if (t->first + s.begin()->first <= a[i] || s.begin()->first + a[i] <= s.rbegin()->first) {
+                    s.erase({a[id], id});  
+                    id++; 
+                } else break;  
+            }
+            s.insert({a[i], i});  
+            if (s.size() > 2)  ans = max(ans, (int)s.size());  
+        }
+    }
+    cout<<ans;
+}
