df322c7c1981c9b6b222d4d0cbdb719d202f0b8c
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-22 11:32:21 +0700

    Update OLP194. BOI SO.cpp

--- LUYEN TAP NANG CAO/OLYMPIC/OLP194. BOI SO.cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP194. BOI SO.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll inf = 1e18;
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> d(n, inf); 
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<>> pq;
+    for (ll i = 1; i < 10; ++i) {
+        if (i < n) {
+            d[i] = i;
+            pq.push({i, i});
+        }
+    }
+    while (!pq.empty()) {
+        ll w = pq.top().first, u = pq.top().second;
+        pq.pop();
+        if (w != d[u]) continue;
+        for (ll i = 0; i < 10; ++i) {
+            ll x = (u * 10 + i) % n;
+            if (d[x] > d[u] + i) {
+                d[x] = d[u] + i;
+                pq.push({d[x], x});
+            }
+        }
+    }
+    cout << d[0];
+}
 
