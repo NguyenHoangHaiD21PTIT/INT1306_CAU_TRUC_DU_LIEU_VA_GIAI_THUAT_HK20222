c17eb5dea377f04b6e745c5e0ea50bb6ac2095dd
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-30 23:31:12 +0700

    Update DSA_P039. CHON O - 1.cpp

--- KIỂM TRA, KHẢO SÁT D22/DSA_P039. CHON O - 1.cpp
+++ KIỂM TRA, KHẢO SÁT D22/DSA_P039. CHON O - 1.cpp
@@ -1,40 +1,33 @@
-
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+const int INF = -1e9, MAXN = 22; // Kích thước ma trận tối đa
+int n, t, a[MAXN][MAXN]; // Ma trận và kích thước
+int dp[21][1 << 20]; // Mảng dp
 
-const int oo = 1e18 + 7;
-int n, a[16][16], f[1 << 16][16];
-
-void solve() {
-    memset(f, 0, sizeof(f));
-    cin >> n;
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j) cin >> a[i][j];
-
-    int x = 1 << n;
-    for (int mask = 1; mask < x; ++mask) {
-        if (__builtin_popcount(mask) > 1) {
-            for (int i = 0; i < n; ++i) {
-                if ((mask >> i) & 1) {
-                    int pre_mask = mask ^ (1 << i);
-                    f[mask][i] = oo;
-                    for (int j = 0; j < n; ++j) {
-                        if ((pre_mask >> j) & 1) {
-                            f[mask][i] = min(f[mask][i], f[pre_mask][j] + a[j][i]);
+signed main() {
+    cin >> t; 
+    while (t--) { 
+        cin >> n;
+        for (int i = 1; i <= n; i++) {  
+            for (int j = 0; j < n; j++) cin >> a[i][j]; 
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (1 << n); j++) dp[i][j] = 0;
+        }
+        for (int i = 0; i < n; ++i) dp[1][1 << i] = a[1][i];
+        for (int i = 1; i < n; i++) {  // Xét đến hàng thứ i
+            for (int state = 0; state < (1 << n); state++) { // Xét mọi trạng thái hàng trước đó
+                if (dp[i][state]) { // Kiểm tra trạng thái hợp lệ
+                    for (int j = 0; j < n; j++) { // Lần lượt bật từng bit hàng đang xét lên
+                        if ((state & (1 << j)) == 0) { // Nếu cột j chưa được chọn ở hàng trước
+                            int new_state = state | (1 << j); 
+                            dp[i + 1][new_state] = max(dp[i + 1][new_state], dp[i][state] + a[i + 1][j]); 
                         }
                     }
                 }
             }
         }
+        cout << dp[n][(1 << n) - 1] << '\n'; 
     }
-
-    int res = oo;
-    for (int i = 0; i < n; ++i) res = min(res, f[x - 1][i]);
-    cout << res << '\n';
-}
-
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int TC; cin >> TC;
-    while (TC--) solve();
 }
