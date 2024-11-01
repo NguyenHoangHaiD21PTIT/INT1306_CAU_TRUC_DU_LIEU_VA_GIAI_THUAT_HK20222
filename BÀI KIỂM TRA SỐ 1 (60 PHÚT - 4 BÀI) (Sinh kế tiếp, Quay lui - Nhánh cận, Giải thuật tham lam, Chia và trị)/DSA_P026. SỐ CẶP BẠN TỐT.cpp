2502807ff2c2b9a60474993e4d6bac2b165b6451
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-01 07:50:40 +0700

    Update DSA_P026. SỐ CẶP BẠN TỐT.cpp

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P026. SỐ CẶP BẠN TỐT.cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P026. SỐ CẶP BẠN TỐT.cpp
@@ -1,44 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    long n, k, A[25], B[60005], i;
+    long n, k;
+    vector<long> A(25, 0);  
+    vector<long> B(60005, 0);
     long long S = 0;
-    char x[30];
-
-    scanf("%ld %ld", &n, &k);
-
-    for (i = 1; i <= n; i++) {
-        scanf("%s", x);
-        B[i] = strlen(x);
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        string x;
+        cin >> x;
+        B[i] = x.length();
     }
-
-    for (i = n + k; i > n; i--) {
-        B[i] = 0;
-    }
-
-    for (i = 2; i <= 20; i++) {
-        A[i] = 0;
-    }
-
+    for (int i = 2; i <= 20; i++) A[i] = 0;
     if (k < n) {
-        for (i = k; i >= 1; i--) {
-            A[B[i]]++;
-        }
+        for (int i = k; i >= 1; i--) A[B[i]]++;
     } else {
-        for (i = n; i >= 1; i--) {
-            A[B[i]]++;
-        }
+        for (int i = n; i >= 1; i--) A[B[i]]++;
     }
-    
-    for (i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         A[B[i + k]]++;
         S += A[B[i]] - 1;
         A[B[i]]--;
     }
-
-    // In ra tổng số cặp bạn tốt
-    printf("%lld", S);
-
-    return 0;
+    cout << S << endl;
 }
