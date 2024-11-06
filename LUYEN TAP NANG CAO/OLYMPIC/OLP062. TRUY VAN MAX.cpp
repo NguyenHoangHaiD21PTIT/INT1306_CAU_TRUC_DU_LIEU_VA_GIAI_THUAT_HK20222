c04f688c5a2e04f8fa42fa66b912a5df9bd6a00b
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-06 10:30:09 +0700

    Update OLP062. TRUY VAN MAX.cpp

--- LUYEN TAP NANG CAO/OLYMPIC/OLP062. TRUY VAN MAX.cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP062. TRUY VAN MAX.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 const int MAXN = 100000;
-int t[4 * MAXN], lazy[4 * MAXN], A[MAXN], N, M;               
+long long t[4 * MAXN], lazy[4 * MAXN], A[MAXN];
+long long N, M;               
 
-void build(int v, int l, int r) {
+void build(long long v, long long l, long long r) {
     if (l == r) {
         t[v] = A[l]; 
     } else {
-        int m = (l + r) / 2;
+        long long m = (l + r) / 2;
         build(2 * v, l, m);            
         build(2 * v + 1, m + 1, r);   
         t[v] = max(t[2 * v], t[2 * v + 1]); 
     }
 }
 
-void propagate(int v, int l, int r) {
+void propagate(long long v, long long l, long long r) {
     if (lazy[v] != 0) {
         t[v] += lazy[v]; 
         if (l != r) {    
@@ -26,36 +27,35 @@ void propagate(int v, int l, int r) {
     }
 }
 
-
-void update(int v, int l, int r, int u, int v_end, int x) {
+void update(long long v, long long l, long long r, long long u, long long v_end, long long x) {
     propagate(v, l, r); 
     if (v_end < l || u > r) return; 
     if (u <= l && r <= v_end) { 
         lazy[v] += x;
         propagate(v, l, r);
     } else { 
-        int m = (l + r) / 2;
+        long long m = (l + r) / 2;
         update(2 * v, l, m, u, v_end, x);
         update(2 * v + 1, m + 1, r, u, v_end, x);
         t[v] = max(t[2 * v], t[2 * v + 1]); 
     }
 }
 
-
-int query(int v, int l, int r, int u, int v_end) {
+long long query(long long v, long long l, long long r, long long u, long long v_end) {
     propagate(v, l, r); 
-    if (v_end < l || u > r) return INT_MIN;
+    if (v_end < l || u > r) return LLONG_MIN;
     if (u <= l && r <= v_end) return t[v]; 
-    int m = (l + r) / 2;
+    long long m = (l + r) / 2;
     return max(query(2 * v, l, m, u, v_end), query(2 * v + 1, m + 1, r, u, v_end));
 }
 
 int main() {
     cin >> N >> M;
-    for (int i = 1; i <= N; i++) cin >> A[i];
+    for (long long i = 1; i <= N; i++) cin >> A[i];
     build(1, 1, N);
-    for (int i = 0; i < M; i++) {
-        int type, u, v, x;
+    for (long long i = 0; i < M; i++) {
+        long long type, u, v;
+        long long x;
         cin >> type >> u >> v;
         if (type == 1) cout << query(1, 1, N, u, v) << "\n";
         else if (type == 2) {
