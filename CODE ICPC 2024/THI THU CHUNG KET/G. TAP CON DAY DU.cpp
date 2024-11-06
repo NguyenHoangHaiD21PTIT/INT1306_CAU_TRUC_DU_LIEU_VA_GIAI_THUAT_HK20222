7942ff4c90cdee9d8653de78058a34a87e66a296
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-07 04:56:31 +0700

    Update G. TAP CON DAY DU.cpp

--- CODE ICPC 2024/THI THU CHUNG KET/G. TAP CON DAY DU.cpp
+++ CODE ICPC 2024/THI THU CHUNG KET/G. TAP CON DAY DU.cpp
@@ -1,32 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, res = 0;  
-vector<int> bitmask;  // Lưu bitmask của từng từ
+int N, a[30], cnt = 0, ok; 
+vector<int> mask;  
+vector<string> word; 
+
+void sinh() {
+    int i = N - 1;
+    while (i >= 0 && a[i] == 1) {
+        a[i] = 0;
+        i--;
+    }
+    if (i == -1) ok = 0;
+    else a[i] = 1;
+}
 
 int get(string word) {
     int mask = 0;
-    for (char c : word) mask |= (1 << (c - 'a'));  // Bật bit tương ứng với ký tự c
+    for (char c : word) mask |= (1 << (c - 'a'));  
     return mask;
 }
 
-void Try(int idx, int curMask) {
-    if (idx == N) {
-        if (curMask == (1 << 26) - 1) res++;
-        return;
-    }
-    Try(idx + 1, curMask);
-    Try(idx + 1, curMask | bitmask[idx]);
-}
-
 int main() {
-    cin >> N;
-    bitmask.resize(N);
+    cin >> N;  
+    mask.resize(N); word.resize(N);  
     for (int i = 0; i < N; i++) {
-        string word;
-        cin >> word;
-        bitmask[i] = get(word);  
+        cin >> word[i];  
+        mask[i] = get(word[i]);  
     }
-    Try(0, 0);
-    cout << res << endl;
+    memset(a, 0, sizeof(a));
+    ok = 1;
+    while (ok) {
+        int xorMask = 0;
+        for (int i = 0; i < N; i++) {
+            if (a[i] == 1) xorMask |= mask[i];
+        }
+        if (xorMask == (1 << 26) - 1) cnt++;
+        sinh();
+    }
+    cout << cnt;
 }
