07a9353100c641cc91740c0d891672bd967c70a0
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-13 22:21:38 +0700

    Create S130. CAP SO DAC BIET.cpp

---
+++ LUYEN TAP NANG CAO/S130. CAP SO DAC BIET.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mxn = 1e5 + 7;
+int f[mxn];
+
+int get(int x) {
+    int res = 0;
+    while (x < mxn) {
+        res += f[x];
+        x += x & -x;
+    }
+    return res;
+}
+
+void update(int x) {
+    while (x > 0) {
+        ++f[x];
+        x -= x & -x;
+    }
+}
+
+void solve() {
+    int n; cin >> n;
+    int a[n + 5];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    int res = 0;
+    vector<int> v;
+    for (int i = 1; i <= n; i++) {
+        if (a[i] % 2 == 0) {
+            res += get(a[i] + 1);  
+            v.push_back(a[i]);
+        } else {
+            for (int num : v) update(num);  
+            v.clear();
+        }
+    }
+    cout << res << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int TC = 1;
+    while (TC--) solve();
+}
