c4565aec3843594b104d3696608b08128c7b11f0
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-16 13:45:08 +0700

    Update DSA_P112. VI TRI XUAT PHAT.cpp

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P112. VI TRI XUAT PHAT.cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P112. VI TRI XUAT PHAT.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, a[100007];
+bool check(int x) {//Kiểm tra xem có thể xếp m tay đua sao cho kcach min giữa các tay đua >=x
+    int cnt = 1, last = 1;//cnt: số xe đã xếp vị trí, last: vị trí của xe cuối cùng được xếp
+    for (int i = 2; i <= n; i++) {
+        if (a[i] - a[last] >= x) {
+            ++cnt;
+            if (cnt == m) return true;
+            last = i;
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    int l = 0, r = 1e9, res = 0;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (check(mid)) {
+            res = mid;
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    cout << res;
+}
 
