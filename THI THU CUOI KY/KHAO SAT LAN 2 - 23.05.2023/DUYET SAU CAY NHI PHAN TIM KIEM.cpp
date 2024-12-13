a007a15892490b269641658bc5124b674c373e5a
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-13 19:55:06 +0700

    Create DUYET SAU CAY NHI PHAN TIM KIEM.cpp

---
+++ THI THU CUOI KY/KHAO SAT LAN 2 - 23.05.2023/DUYET SAU CAY NHI PHAN TIM KIEM.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    int data;
+    node* left;
+    node* right;
+    node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+
+node *insert_node(node *root, int u) {
+	if (root == NULL) root = new node(u);
+	else {
+		if (u < root->data) root->left = insert_node(root->left, u);
+		else root->right = insert_node(root->right, u);
+	}
+	return root;
+}
+
+void postOrder(node *root) {
+	if (root == NULL) return;
+	postOrder(root->left);
+	postOrder(root->right);
+	cout << root->data << " ";
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while (t--) {
+		node *root = NULL;
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			int x; cin >> x;
+			root = insert_node(root, x);
+		}
+		postOrder(root);
+		cout << endl;
+	}
+}
