e764913cfdea4e2587e3550ee41553d72b871440
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-06-07 16:09:19 +0700

    Cập nhật việc reset sau mỗi testcase

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P016. SAP XEP QUAN HAU.cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P016. SAP XEP QUAN HAU.cpp
@@ -20,6 +20,10 @@ int main(){
 	int t;
 	cin>>t;
 	for(int k = 1;k<=t;k++){
+		res = 0;
+		memset(cot, 0, sizeof(cot));
+		memset(xuoi, 0, sizeof(xuoi));
+		memset(nguoc, 0, sizeof(nguoc));
 		for(int i=1;i<=8;i++){
 			for(int j=1;j<=8;j++) cin>>a[i][j];
 		}
