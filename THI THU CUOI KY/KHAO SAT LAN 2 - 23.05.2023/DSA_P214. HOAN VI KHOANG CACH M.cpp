c3a2474cea06a11ed0d6c5269c23b9a0b7b15303
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-09 23:01:00 +0700

    Đổi tên

--- THI THU CUOI KY/KHAO SAT LAN 2 - 23.05.2023/DSA_P214. HOAN VI KHOANG CACH M.cpp
+++ THI THU CUOI KY/KHAO SAT LAN 2 - 23.05.2023/DSA_P214. HOAN VI KHOANG CACH M.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
 	int n, k;
 	cin>>n>>k;
-    int cnt = 1;
+    	int cnt = 1;
 	int a[n];
 	for(int i = 0;i<n;i++) a[i] = i + 1;
 	while(next_permutation(a, a+n)){
-        cnt++;
+        	cnt++;
 		if(cnt%k==0){
-            for(int i = 0;i<n;i++) cout<<a[i]<<" ";
-		    cout<<endl;
-        }
+            		for(int i = 0;i<n;i++) cout<<a[i]<<" ";
+			cout<<endl;
+        	}
 	}
 }
