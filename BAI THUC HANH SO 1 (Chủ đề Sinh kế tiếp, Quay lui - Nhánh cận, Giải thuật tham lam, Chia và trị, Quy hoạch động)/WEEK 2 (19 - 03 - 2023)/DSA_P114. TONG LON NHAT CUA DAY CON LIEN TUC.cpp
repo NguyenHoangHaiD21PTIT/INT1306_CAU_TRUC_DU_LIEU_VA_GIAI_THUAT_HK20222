65f18655cd5c6c727fc75301eb7e2b58d5c40838
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-09 18:50:03 +0700

    Giải thích vì sao phải lấy dấu bằng 1 phía

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P114. TONG LON NHAT CUA DAY CON LIEN TUC.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P114. TONG LON NHAT CUA DAY CON LIEN TUC.cpp
@@ -38,3 +38,12 @@ int main(){
 /*Nếu xong bài này thì có thể luyện tập tiếp 2 bài sau trên Leetcode:
 Bài 1: 907. SUM OF SUB ARRAY MINUMUMS https://leetcode.com/problems/sum-of-subarray-minimums/description/?envType=daily-question&envId=2024-01-20
 Bài 2: 2104. SUM OF SUBARRAY RANGES https://leetcode.com/problems/sum-of-subarray-ranges/ */
+/* Ví dụ: Dãy 2 1 2
+Nếu giữ dấu bằng cả 2 phía thì
+i 0 1 2
+left  -1 0 -1 
+right 3 2 3 
+Xét phần tử a[0]. Chọn điểm đầu từ 0 đến 0, 1 cách. Chọn điểm cuối từ 0 đến 2, 3 cách. Ta có 3 dãy: 2, 2 1 và 2 1 2
+Xét phần tử a[2]. Chọn điểm đầu từ 2 đến 0, 3 cách. Chọn điểm cuối từ 2 đến 2, 1 cách. Ta có 3 dãy: 2, 1 2 và 2 1 2
+Như vậy là trùng nhau 
+*/
