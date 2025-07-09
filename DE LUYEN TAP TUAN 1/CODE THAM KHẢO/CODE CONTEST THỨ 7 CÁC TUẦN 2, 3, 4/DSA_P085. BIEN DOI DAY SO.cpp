632742f9212bef30fc6049b413d9518e5fd67735
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-09 17:31:38 +0700

    Cập nhật đề bài đúng và giải thích code

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P085. BIEN DOI DAY SO.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P085. BIEN DOI DAY SO.cpp
@@ -10,12 +10,15 @@ int main() {
         for (int i = 0; i < N; i++) cin >> A[i] >> B[i];
         long long dp[N][3];
         for(int i = 0; i < 3; i++) dp[0][i] = LLONG_MAX;
+        //dp[i][j]: Chi phí để biến đổi i số với số thứ i được tăng lên j đơn vị
         dp[0][0] = 0; 
         dp[0][1] = B[0]; 
         dp[0][2] = 2 * B[0];
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < 3; j++) {
                 dp[i][j] = LLONG_MAX; 
+                //Trước đó, ta đã có chi phí NHỎ NHẤT để biến đổi i-1 số với số thứ i-1 được tăng lên j đơn vị rồi
+                //Bây giờ, ta sẽ thử tăng số thứ i (Số hiện tại) lên k đơn vị. Chọn giá trị tối ưu nhất trong 9 tổ hợp
                 for (int k = 0; k < 3; k++) {
                     if (A[i] + j != A[i - 1] + k) dp[i][j] = min(dp[i][j], dp[i - 1][k] + j * B[i]);
                 }
