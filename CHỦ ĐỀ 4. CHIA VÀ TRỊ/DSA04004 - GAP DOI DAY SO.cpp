8bc49ae60d918476dad384ba4bdf691024394d89
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 19:41:43 +0700

    Update DSA04004 - GAP DOI DAY SO.cpp

--- CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04004 - GAP DOI DAY SO.cpp
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04004 - GAP DOI DAY SO.cpp
@@ -4,13 +4,9 @@ using namespace std;
 long long find(long long n, long long k){
 	if(k==1) return 1;
 	long long res = pow(2,n-1);
-	if(k==res){
-		return n;
-	} else if (k< res){
-		return find(n-1,k);
-	} else {
-		return find(n-1,k-res);
-	}
+	if(k==res) return n;
+	else if (k< res) return find(n-1,k);
+	else return find(n-1,k-res);
 }
 
 int main(){
