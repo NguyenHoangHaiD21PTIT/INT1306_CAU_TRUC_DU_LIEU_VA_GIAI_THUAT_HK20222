b3e9c38695ef677249eb4344d03f4992f84c4fbe
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 19:42:52 +0700

    Update DSA04005 - DAY XAU FIBONACCI.cpp

--- CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04005 - DAY XAU FIBONACCI.cpp
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04005 - DAY XAU FIBONACCI.cpp
@@ -12,11 +12,8 @@ void fibo(){
 char find(long long n, long long k){
 	if(n==1) return 'A';
 	if(n==2) return 'B';
-	if(k<=f[n-2]){
-		return find(n-2,k);
-	} else {
-		return find(n-1,k-f[n-2]);
-	}
+	if(k<=f[n-2]) return find(n-2,k);
+	else return find(n-1,k-f[n-2]);
 }
 
 int main(){
