ac74207a6c3a4bfce1161a2cf74006a4c0a7ff86
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 19:29:16 +0700

    Update DSA03002 - NHAM CHU SO.cpp

--- CHỦ ĐỀ 3. GIẢI THUẬT THAM LAM/DSA03002 - NHAM CHU SO.cpp
+++ CHỦ ĐỀ 3. GIẢI THUẬT THAM LAM/DSA03002 - NHAM CHU SO.cpp
@@ -3,18 +3,14 @@ using namespace std;
 
 string to(string &s){
 	for(int i=0;i<s.size();i++){
-		if(s[i]=='5'){
-			s[i]='6';
-		}
+		if(s[i]=='5') s[i]='6';
 	}
 	return s;
 }
 
 string be(string &s){
 	for(int i=0;i<s.size();i++){
-		if(s[i]=='6'){
-			s[i]='5';
-		}
+		if(s[i]=='6')s[i]='5';
 	}
 	return s;
 }
@@ -22,11 +18,6 @@ string be(string &s){
 int main(){
 	string s1, s2;
 	cin>>s1>>s2;
-	long long max1 = stol(to(s1));
-	long long max2 = stol(to(s2));
-	long long min1 = stol(be(s1));
-	long long min2 = stol(be(s2));
-	long long tongto = max1 + max2;
-	long long tongbe = min1 + min2;
-	cout<<tongbe<<" "<<tongto;
+	long long max1 = stol(to(s1)), max2 = stol(to(s2)), min1 = stol(be(s1)), min2 = stol(be(s2));
+	cout<<min1 + min2<<" "<<max1 + max2;
 }
