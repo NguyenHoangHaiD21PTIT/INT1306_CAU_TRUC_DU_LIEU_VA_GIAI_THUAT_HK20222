7059687928c7a4b306fb0f6a0d1166c196465ff1
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 19:23:14 +0700

    Update DSA_P026. SỐ CẶP BẠN TỐT.cpp

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P026. SỐ CẶP BẠN TỐT.cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P026. SỐ CẶP BẠN TỐT.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-    int n, k, ans = 0;
+    ll n, k, ans = 0;
     cin >> n >> k;
     vector<string> s(n + 1);
-    int f[n + 1][30] = {};  //f[i][j]: Số lượng xâu con dài j tính từ đầu cho đến chỉ số i
-    for (int i = 1; i <= n; i++) {
+    ll f[n + 1][30] = {};  //f[i][j]: Số lượng xâu con dài j tính từ đầu cho đến chỉ số i
+    for (ll i = 1; i <= n; i++) {
         cin >> s[i];
-        for (int j = 0; j < 30; j++) f[i][j] = f[i - 1][j];  
+        for (ll j = 0; j < 30; j++) f[i][j] = f[i - 1][j];  
         f[i][s[i].size()]++;  
     }
     for (int i = 1; i <= n; i++) {
         int length = s[i].size();
-        ans += f[i - 1][length] - f[max(i - k - 1, 0)][length];
+        ans += f[i - 1][length] - f[max(i - k - 1, 0LL)][length];
     }
     cout << ans;
 }
