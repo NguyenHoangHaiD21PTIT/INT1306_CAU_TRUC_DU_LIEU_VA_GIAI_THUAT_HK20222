4c96c4329a5a59df208d12edc50d725b79b0cdf3
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-25 15:40:10 +0700

    Update DSA_P220. NHAY SANG PHAI.cpp

--- D21 - THI CHINH THUC - DEMO/CA 13H/DSA_P220. NHAY SANG PHAI.cpp
+++ D21 - THI CHINH THUC - DEMO/CA 13H/DSA_P220. NHAY SANG PHAI.cpp
@@ -2,26 +2,27 @@
 using namespace std;
 int main(){
     int n, q;
-    cin>>n>>q;
+    cin >> n >> q;
     int a[n];
     for(int i = 0;i<n;i++) cin>>a[i];
     stack<int>st;
     int r[n];
-    for(int i = n - 1;i>=0;i--){
-        while(!st.empty()&&a[i]>=a[st.top()]) st.pop();
-        if(st.empty()) r[i] = -1;
-        else r[i] = st.top();
+    //r[i]: Phần tử đầu tiên bên phải a[i] lớn hơn a[i]
+    for(int i = n - 1;i >= 0;i--){
+        while(!st.empty() && a[i] >= a[st.top()]) st.pop();
+        if(st.empty()) r[i] = -1; // Không có phần tử nào lớn hơn bên phải
+        else r[i] = st.top(); // Ghi nhận chỉ số phần tử lớn hơn tiếp theo
         st.push(i);
     }
     while(q--){
         int index, buoc = 0;
-        cin>>index;
+        cin >> index;
         index--;
         while(r[index]!=-1){
             buoc++;
             index = r[index];
         }
-        cout<<buoc<<endl;
+        cout << buoc << endl;
     }
 }
 /*
