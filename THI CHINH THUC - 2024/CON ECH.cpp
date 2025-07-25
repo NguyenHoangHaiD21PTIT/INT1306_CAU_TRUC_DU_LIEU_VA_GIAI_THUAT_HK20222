cb4f12b6912b523b8641f5e027ac4f001091c1f8
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-25 15:43:40 +0700

    Update CON ECH.cpp

--- THI CHINH THUC - 2024/CON ECH.cpp
+++ THI CHINH THUC - 2024/CON ECH.cpp
@@ -7,9 +7,7 @@ int main() {
 	for (auto& i : a) cin >> i;
 	vector<int> dp(n);
 	dp[0] = 0, dp[1] = abs(a[1] - a[0]);
-	for (int i = 2; i < n; i++) {
-		dp[i] = min(dp[i - 1] + abs(a[i] - a[i - 1]), dp[i - 2] + abs(a[i] - a[i - 2]));
-	}
+	for (int i = 2; i < n; i++) dp[i] = min(dp[i - 1] + abs(a[i] - a[i - 1]), dp[i - 2] + abs(a[i] - a[i - 2]));
 	cout << dp[n - 1] << endl;
 }
 //https://oj.vnoi.info/problem/atcoder_dp_a
