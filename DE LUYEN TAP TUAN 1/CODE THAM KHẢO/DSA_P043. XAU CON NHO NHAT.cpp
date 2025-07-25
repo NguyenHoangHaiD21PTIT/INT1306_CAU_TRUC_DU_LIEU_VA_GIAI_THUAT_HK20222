d7e432af51bd266ac195c01a78d767a3778d155f
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-25 09:04:17 +0700

    Update giải thích

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P043. XAU CON NHO NHAT.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P043. XAU CON NHO NHAT.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin >> t;
+    int t; cin >> t;
     while(t--){
-        string s;
-        cin >> s;
+        string s; cin >> s;
         set<char>sc;
+        // Xác định tập các ký tự trong xâu s
         for(char x: s) sc.insert(x);
-        int st = 0, ans = 1e9, x = 0, a[256] = {};
+        int st = 0, ans = 1e9, x = 0, a[256] = {};//st: Chỉ số bắt đầu đoạn con đang xét, x: Số ký tự khác nhau trong xâu con đang xét
         for(int i = 0;i<s.size();i++){
             a[s[i]]++;
-            if(a[s[i]]==1) x++;
-            if(x==sc.size()){
-                while(a[s[st]]>1){
+            if(a[s[i]] == 1) x++;//đếm = 1 --> Lần đầu xuất hiện --> Ghi nhận là có thêm 1 ký tự phân biệt
+            if(x == sc.size()){ //Có đủ mọi ký tự trong xâu s rồi
+                //Cố gắng thu gọn về bên phải nhất có thể
+                while(a[s[st]] > 1){
                     a[s[st]]--;
                     st++;
                 }
                 ans = min(ans, i - st + 1);
             }
         }
-        cout<<ans<<endl;
+        cout << ans << endl;
     }
 }
