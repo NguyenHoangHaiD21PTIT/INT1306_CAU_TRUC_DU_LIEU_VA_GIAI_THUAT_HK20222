46d9c475f291c592378286d267269de5c4af258e
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-25 15:33:29 +0700

    Cập nhật giải thích

--- KIỂM TRA, KHẢO SÁT D22/DSA_P124. TỔNG BẰNG 0.cpp
+++ KIỂM TRA, KHẢO SÁT D22/DSA_P124. TỔNG BẰNG 0.cpp
@@ -9,10 +9,12 @@ int main() {
     mp[0] = 1; 
     long long sum = 0;
     for(int i = 1; i <= n; i++) {
+        //sum: a[1] + a[2] + ... + a[i]
         sum += a[i];
+        //mp[sum]: Số lượng chỉ số j phân biệt < i sao cho tổng: a[1] + a[2] + ... + a[j] bằng sum 
         cnt += mp[sum];
+        //Ghi nhận là có thêm 1 index i nữa mà a[1] + a[2] + ... + a[i] = sum
         mp[sum]++;
     }
     cout << cnt;
 }
-
