39e784ebfe239b935813e03f20ed89384a0a8dc7
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-08 09:18:13 +0700

    Update DSA_P002. LIET KE HOAN VI - 1.cpp

--- BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P002. LIET KE HOAN VI - 1.cpp
+++ BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P002. LIET KE HOAN VI - 1.cpp
@@ -1,28 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a[100], ok;
-void kt(){
-	for(int i=0;i<n;i++){
-		a[i]=i+1;
-	}
-}
 int main(){
-		cin>>n;
-		kt();
-		int cnt = 1;
-		//In cau hinh dau tien
-		cout<<"1: ";
-		for(int i=0;i<n;i++){
-			cout<<a[i]<<" ";
-		}
-		cout<<endl;
-		//Kiem tra con sinh duoc tiep hay khong roi in tiep
-		while(next_permutation(a,a+n)!=false){
-			cnt++;
-			cout<<cnt<<": ";
-			for(int i=0;i<n;i++){
-				cout<<a[i]<<" ";
-			}
-			cout<<endl;
-		}
+	int n, a[100], ok; cin >> n; 
+	for(int i = 0;i < n; i++) a[i] = i+1;
+	int cnt = 1;
+	cout << "1: ";
+	for(int i = 0;i < n; i++) cout << a[i] << " ";
+	cout << endl;
+	while(next_permutation(a, a + n)){
+		cnt++;
+		cout << cnt << ": ";
+		for(int i = 0;i < n; i++) cout << a[i] << " ";
+		cout << endl;
+	}
 }
