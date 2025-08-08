c8bc9fe04f59f05518ccb0371b3c4b5788c1e375
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-08 10:00:23 +0700

    Update DSA_P093. KY TU A - B - C.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P093. KY TU A - B - C.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P093. KY TU A - B - C.cpp
@@ -1,55 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, a[100], ok;
-
-//Sinh chinh hop lap chap n cua 3. 
-void kt(){
-	for(int i=0;i<n;i++) a[i] = 1;
+bool check(const string &s) {
+    int a = 0, b = 0, c = 0;
+    for (char ch : s) {
+        if (ch == 'A') ++a;
+        else if (ch == 'B') ++b;
+        else ++c;
+    }
+    return a && b && c && a <= b && b <= c;
 }
 
-void sinh(){
-	int i = n-1;
-	while(i>=0&&a[i]==3) i--;
-	if(i==-1){
-		ok = 0;
-	} else {
-		a[i]++;
-		for(int j=i+1;j<n;j++) a[j] = 1;
-	}
+void gen(int len) {
+    vector<int> a(len, 1); //Dãy khởi tạo toàn số 1
+    string ch = "ABC";
+    bool ok = true;
+    while (ok) {
+        string tmp;
+        for (int i : a) tmp += ch[i - 1];
+        if (check(tmp)) cout << tmp << '\n';
+        int i = len - 1;
+        while (i >= 0 && a[i] == 3) --i;
+        if (i < 0) ok = false;
+        else {
+            a[i]++;
+            for (int j = i + 1; j < len; j++) a[j] = 1;
+        }
+    }
 }
 
-int check(string s){
-	int cnta = 0, cntb = 0, cntc = 0;
-	for(int i=0;i<s.size();i++){
-		if(s[i]=='A') cnta++;
-		else if (s[i]=='B') cntb++;
-		else if (s[i]=='C') cntc++;
-	}
-	if(cnta!=0&&cntb!=0&&cntc!=0&&cnta<=cntb&&cntb<=cntc) return 1;
-	else return 0;
-}
-
-int main(){
-	int n1;
-	cin>>n1;
-	n = 3;
-	string s="ABC";
-	cout<<s<<endl;
-	while(next_permutation(s.begin(),s.end())!=false) cout<<s<<endl;
-	n = 4;
-	while(n<=n1){
-		kt();
-		ok=1;
-		while(ok==1){
-			string tmp="";
-			for(int i = 0;i<n;i++){
-				int val = a[i] - 1;
-				tmp = tmp + s[val];
-			}
-			if(check(tmp)) cout<<tmp<<endl;
-			sinh();
-		}
-		n++;
-	}
+int main() {
+    int n; cin >> n;
+    string s = "ABC";
+    cout << s << '\n';
+    while (next_permutation(s.begin(), s.end())) cout << s << '\n';
+    for (int len = 4; len <= n; ++len) gen(len);
 }
