1b9f4bebe14a23f309bbc74279aeb3d9e96616ab
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-08 11:22:01 +0700

    Update DSAKT017. DAY XAU NHI PHAN.cpp

--- KIỂM TRA, KHẢO SÁT D22/DSAKT017. DAY XAU NHI PHAN.cpp
+++ KIỂM TRA, KHẢO SÁT D22/DSAKT017. DAY XAU NHI PHAN.cpp
@@ -1,1 +1,22 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+long long f[100];
+void fibo(){
+	f[1] = 1; f[2] = 1;
+	for(long long i = 3;i <= 92; i++) f[i] = f[i - 2] + f[i - 1];
+}
+char find(long long n, long long k){
+	if(n == 1) return '0';
+	if(n == 2) return '1';
+	if(k <= f[n-2]) return find(n - 2, k);
+	else return find(n - 1,k - f[n - 2]);
+}
+int main(){
+	int t; cin >> t;
+	fibo();
+	while(t--){
+		long long n, k;
+		cin >> n >> k;
+		cout << find(n, k) << endl;
+	}
+}
