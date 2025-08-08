056850f18daad93f64b8877ca08d80104d90ed2c
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-08 09:22:14 +0700

    Update DSA_P015. LIET KE HOAN VI - 2.cpp

--- LUYEN TAP SINH KE TIEP - HOC KY HE 2023.3/DSA_P015. LIET KE HOAN VI - 2.cpp
+++ LUYEN TAP SINH KE TIEP - HOC KY HE 2023.3/DSA_P015. LIET KE HOAN VI - 2.cpp
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int n, k;
-    cin>>n>>k;
+    int n, k; cin >> n >> k;
     vector<int>a;
-    for(int i = 1;i<=n;i++){
-        if(i!=k) a.push_back(i);
+    for(int i = 1;i <= n;i++){
+        if(i != k) a.push_back(i);
     }
-    for(int x: a) cout<<x<<" ";
-    cout<<k<<endl;
+    for(int x: a) cout << x << " ";
+    cout << k << endl;
     while(next_permutation(a.begin(), a.end())){
-        for(int x: a) cout<<x<<" ";
-        cout<<k<<endl;
+        for(int x: a) cout << x << " ";
+        cout << k << endl;
     }
 }
