3baf32442ea0ee20828e3f2211847d923c42e344
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 08:46:58 +0700

    Update DSA_P006. QUAN VUA TREN BAN CO.cpp

--- CONTEST 1/DSA_P006. QUAN VUA TREN BAN CO.cpp
+++ CONTEST 1/DSA_P006. QUAN VUA TREN BAN CO.cpp
@@ -9,18 +9,18 @@ int visited[10][10], d[10][10];
 int BFS(){
 	memset(visited, 0, sizeof(visited));
 	memset(d, 0, sizeof(d));
-	queue<pair<int,int>>q;
+	queue<pair<int, int>>q;
 	q.push({t, s});
 	visited[t][s] = 1;
 	while(!q.empty()){
-		pair<int,int>p = q.front();
+		pair<int, int>p = q.front();
 		q.pop();
 		int i = p.first, j = p.second;
-		if(i==v&&j==u) return d[v][u];
-		for(int k = 0;k<8;k++){
+		if(i == v && j == u) return d[v][u];
+		for(int k = 0; k < 8; k++){
 			int i1 = i + dx[k];
 			int j1 = j + dy[k];
-			if(i1>=0&&i1<=7&&j1>=0&&j1<=7&&!visited[i1][j1]){
+			if(i1 >= 0 && i1 <= 7 && j1 >= 0 && j1 <= 7 && !visited[i1][j1]){
 				q.push({i1, j1});
 				visited[i1][j1] = 1;
 				d[i1][j1] = d[i][j] + 1;
@@ -31,7 +31,7 @@ int BFS(){
 }
 
 int main(){
-	cin>>s>>t>>u>>v;
-	s--;t--;u--;v--;
-	cout<<BFS();
+	cin >> s >> t >> u >> v;
+	s--; t--; u--; v--;
+	cout << BFS();
 }
