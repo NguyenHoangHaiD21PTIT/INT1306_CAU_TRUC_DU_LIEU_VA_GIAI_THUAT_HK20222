bc578fe75bf9e743103e364bc1f9a89d63358228
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 08:58:10 +0700

    Update DSAKT053 + DSA_P038. SO NGUYEN LON.cpp

--- CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSAKT053 + DSA_P038. SO NGUYEN LON.cpp
+++ CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSAKT053 + DSA_P038. SO NGUYEN LON.cpp
@@ -1,28 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int dp[1005][1005];
-
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
 		string s1, s2;
-		cin>>s1>>s2;
-		memset(dp,0, sizeof(dp));
+		cin >> s1 >> s2;
+		memset(dp, 0, sizeof(dp));
 		int n = s1.size(), m = s2.size();
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=m;j++){
-				if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-				else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j++){
+				if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+				else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 			}
 		}
-		cout<<dp[n][m]<<endl;
+		cout << dp[n][m] << endl;
 	}
 }
-/*
-2
-144615
-4976135
-44
-88 */
+
