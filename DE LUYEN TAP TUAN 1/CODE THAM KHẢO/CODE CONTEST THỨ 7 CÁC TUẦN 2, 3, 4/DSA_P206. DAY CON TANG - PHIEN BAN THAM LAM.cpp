17353b9326333180fa1d5a0fc7cd4d0a34cf8206
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 09:44:03 +0700

    Explain

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P206. DAY CON TANG - PHIEN BAN THAM LAM.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/CODE CONTEST THỨ 7 CÁC TUẦN 2, 3, 4/DSA_P206. DAY CON TANG - PHIEN BAN THAM LAM.cpp
@@ -1,26 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, m, x, y, res, test, sum1, k, d;
-vector<int> a[200005];
-
 int main() {
-    cin >> n;
+    int n; cin >> n;
+    vector<vector<int>> seq(1); // seq[0] không dùng, bắt đầu từ 1
+    int m = 0; // số dãy đã tạo
     for (int i = 1; i <= n; ++i) {
-        cin >> x;
-        k = 0;
-        int d = 1, c = m;
-        while (d <= c) {
-            int mid = (d + c) >> 1;
-            if (a[mid][a[mid].size() - 1] < x) c = mid - 1, k = mid;
-                else d = mid + 1;
+        int x; cin >> x;
+        int pos = 0; // vị trí dãy mà x sẽ nối vào
+        int left = 1, right = m;
+        // Chặt nhị phân trên dãy STT các dãy con hiện có. Mục đích là tìm dãy con có STT nhỏ nhất mà phần tử cuối cùng của nó nhỏ hơn x.
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (seq[mid].back() < x) {
+                pos = mid;
+                right = mid - 1; //Xem có dãy nào có STT nhỏ hơn pos mà phần tử cuối cùng của nó cũng nhỏ hơn x không
+            } else {
+                left = mid + 1;
+            }
         }
-        if (!k) a[++m].push_back(x);
-        else a[k].push_back(x);
+        if (pos == 0) {
+            // Không nối vào đâu được → tạo dãy mới
+            seq.push_back({x});
+            m++;
+        } else {
+            // Nối vào dãy pos
+            seq[pos].push_back(x);
+        }
     }
-    cout << m << endl;
+    cout << m << "\n";
     for (int i = 1; i <= m; ++i) {
-        for (auto v: a[i]) cout << v << ' ';
-        cout << endl;
+        for (int v : seq[i]) cout << v << " ";
+        cout << "\n";
     }
 }
