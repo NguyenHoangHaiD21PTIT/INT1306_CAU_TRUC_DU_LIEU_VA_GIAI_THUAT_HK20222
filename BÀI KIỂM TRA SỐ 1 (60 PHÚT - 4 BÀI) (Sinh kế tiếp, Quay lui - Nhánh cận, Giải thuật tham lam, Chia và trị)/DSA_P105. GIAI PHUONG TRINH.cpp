a766a5ad90cfbdde28eba0dedca398b06c4fceb3
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 10:05:04 +0700

    Update DSA_P105. GIAI PHUONG TRINH.cpp

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P105. GIAI PHUONG TRINH.cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P105. GIAI PHUONG TRINH.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 double f(double A, double B, double C, double x) {
     return A * x * x * x + B * x - C;
 }
-
 double solve(double A, double B, double C) {
     double left = 0, right = 1e9;
     double mid;
@@ -13,10 +11,8 @@ double solve(double A, double B, double C) {
         if (f(A, B, C, mid) * f(A, B, C, left) <= 0) right = mid;
         else left = mid;
     }
-
     return (left + right) / 2;
 }
-
 int main() {
     int T;
     cin >> T;
