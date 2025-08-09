1977ca0705c4bf4bf59fb025504941898f99b232
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 09:26:30 +0700

    Update DSA_P110. TONG GIA TRI LON NHAT.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P110. TONG GIA TRI LON NHAT.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P110. TONG GIA TRI LON NHAT.cpp
@@ -1,19 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int dp[6000][6000]={}; //dp[i][j] la GTLN cua cai tui trong luong j khi mang i do vat
-
+int dp[6000][6000] = {}; //dp[i][j] là GTLN của cái túi trọng lượng j với i đồ vật
 int main(){
-	memset(dp,0,sizeof(dp));
-	int n, s; //n: so do vat, s: khoi luong cai tui
-	cin>>n>>s;
+	memset(dp, 0, sizeof(dp));
+	int n, s; //n: số lượng đồ vật, s: trọng lượng tối đa của túi
+	cin >> n >> s;
 	int w[6000], v[6000];//w: khoi luong, v: gia tri
-	for(int i=1;i<=n;i++) cin>>w[i]>>v[i];
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=s;j++){
-			dp[i][j] = dp[i-1][j];
-			if(j>=w[i]) dp[i][j] = max (dp[i][j], dp[i-1][j-w[i]] + v[i]);
+	for(int i = 1; i <= n; i++) cin >> w[i] >> v[i];
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= s; j++){
+			dp[i][j] = dp[i - 1][j];
+			if(j >= w[i]) dp[i][j] = max (dp[i][j], dp[i - 1][j - w[i]] + v[i]);
 		}
 	}
-	cout<<dp[n][s]<<endl;
+	cout << dp[n][s] << endl;
 }
