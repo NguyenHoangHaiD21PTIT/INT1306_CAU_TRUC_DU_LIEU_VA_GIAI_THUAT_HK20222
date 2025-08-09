15f539179d424decd3e64e2e9487f3f56a1b5ad6
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 10:01:04 +0700

    Update DSA_P022. DEM KI TU A.cpp

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P022. DEM KI TU A.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P022. DEM KI TU A.cpp
@@ -1,33 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 long long f[50], cnt[50];
-
 void gen(){ //cnt[i]: Số ký tự A trong xâu thứ i, f[i]: Độ dài xâu thứ i
-	f[0]=1;
-	f[1]=1;
-	cnt[0]=1;
-	cnt[1]=0;
-	for(int i=2;i<=50;i++){
-		f[i] = f[i-1]+f[i-2];
-		cnt[i]=cnt[i-1] + cnt[i-2];
+	f[0] = 1; f[1] = 1; cnt[0] = 1; cnt[1] = 0;
+	for(int i = 2;i <= 50;i++){
+		f[i] = f[i - 1] + f[i - 2];
+		cnt[i] = cnt[i - 1] + cnt[i - 2];
 	}
 }
-
 long long count(long long n, long long k){ //count(n, k): Đếm số ký tự A trong k vị trí đầu tiên của xâu thứ n
-	if(n==0) return 1;
-	if(n==1) return 0;
-	if(k<=f[n-1]) return count(n-1,k);
-	else return cnt[n-1] + count(n-2,k-f[n-1]);
+	if(n == 0) return 1;
+	if(n == 1) return 0;
+	if(k <= f[n - 1]) return count(n - 1, k);
+	else return cnt[n - 1] + count(n - 2, k - f[n - 1]);
 }
-
 int main(){
 	gen();
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
 		long long n, k;
-		cin>>n>>k;
-		cout<<count(n,k)<<endl;
+		cin >> n >> k;
+		cout << count(n,k) << endl;
 	}
 }
