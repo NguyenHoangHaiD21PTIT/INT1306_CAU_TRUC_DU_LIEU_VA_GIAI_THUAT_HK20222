596948956cb7ba94c5da6ba20d77698792520e7b
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-09 09:14:12 +0700

    Update DSA_P095. DAY CON K PHAN TU.cpp

--- BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P095. DAY CON K PHAN TU.cpp
+++ BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P095. DAY CON K PHAN TU.cpp
@@ -1,48 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int n, k, a[100], ok, b[100];
-
 void kt(){
-	for(int i=0;i<k;i++){
-		a[i] = i+1;
-	}
+	for(int i = 1; i <= k; i++) a[i] = i;
 }
-
 void sinh(){
-	int i = k-1;
-	while(i>=0&&a[i]==n - k + (i+1)){
-		i--;
-	}
-	if(i==-1){
-		ok = 0;
-	} else {
+	int i = k;
+	while(i >= 1 && a[i] == n - k + i) i--;
+	if(i == 0) ok = 0;
+	else {
 		a[i]++;
-		for(int j = i+1;j<k;j++){
-			a[j] = a[j-1] + 1;
-		}
+		for(int j = i + 1; j <= k; j++) a[j] = a[j - 1] + 1;
 	}
 }
-
 int main(){
-	cin>>n>>k;
-	for(int i=0;i<n;i++){
-		cin>>b[i];
-	}
-	sort(b,b+n);
-	kt();
-	ok=1;
+	cin >> n >> k;
+	for(int i = 1; i <= n; i++) cin >> b[i];
+	sort(b + 1, b + n + 1);
+	kt(); ok = 1;
 	int ans = 0;
-	vector<string>v;
-	while(ok==1){
-		string s="";
-		for(int i=0;i<k;i++){
-			int pos = a[i] - 1;
-			s = s + to_string(b[pos]) + " ";
-		}
+	vector <string> v;
+	while(ok == 1){
+		string s = "";
+		for(int i = 1; i <= k;i++) s += to_string(b[a[i]]) + " ";
 		v.push_back(s);
 		sinh();
 	}
-	sort(v.begin(),v.end());
-	for(auto i: v) cout<<i<<endl;
+	sort(v.begin(), v.end());
+	for(auto i: v) cout << i << endl;
 }
