bcd5087ceac038e5a077f703c5797c1b02a4aa11
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-13 08:33:25 +0700

    Update sol AC

--- KY NANG XU LY BAI TAP NHIEU SUBTASK/OLP004. CAT GIAY.cpp
+++ KY NANG XU LY BAI TAP NHIEU SUBTASK/OLP004. CAT GIAY.cpp
@@ -3,6 +3,21 @@ using namespace std;
 const int MAX_SIZE = 6005;
 const int MOD = 1e9 + 7;
 
+ // Hàm kiểm tra: có tồn tại hình vuông cạnh s (s = k*edge) có <= e ô tô màu?
+bool ok(int s, int n, int m, int e, int t[6005][6005]) {
+    if (s <= 0 || s > n || s > m) return false;
+    for (int i = s; i <= n; ++i) {
+        // end: i, len: s = en - st + 1 = i - st + 1 --> st = i - s + 1
+        int i0 = i - s;
+        for (int j = s; j <= m; ++j) {
+            int j0 = j - s;
+            int sum = t[i][j] - t[i0][j] - t[i][j0] + t[i0][j0];
+            if (sum <= e) return true; 
+        }
+    }
+    return false; 
+}
+
 int main() {
     int n, m, k, edge, e; //n: chiều ngang, m: chiều dọc, k: c ô vuông đã tô màu, edge: Cạnh hình vuông phải chia hết cho edge, e: số ô được tô màu tối đa
     cin >> n >> m >> k >> edge >> e;
@@ -15,28 +30,17 @@ int main() {
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j)  t[i][j] = t[i - 1][j] + t[i][j - 1] - t[i - 1][j - 1] + a[i][j];
     }
-    int res = 0; // Kết quả tối ưu
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            //Với mỗi ô (i, j). Ta sẽ xét các ma trận con có góc phải dưới là ô này. Chặt nhị phân theo kích thước của ma trận vuông
-            //Cạnh to nhất cũng chỉ được phép là min(i, j). VD (i, j) = (4, 3) thì 3 - x + 1>=1-->x<=3
-            //Đương nhiên để chia hết cho edge, thì tối thiểu phải là edge. 
-            int d = max(res, edge);
-            int c = min(i, j);
-            // Tìm kích thước ma trận vuông tối đa có thể với tổng <= e
-            while (d <= c) {
-                int mid = (d + c) / 2;
-                int x = i - mid + 1;
-                int y = j - mid + 1;
-                int sum = t[i][j] - t[i][y - 1] - t[x - 1][j] + t[x - 1][y - 1];
-                if (sum <= e) {
-                    res = max(res, mid);
-                    d = mid + 1;
-                } else {
-                    c = mid - 1;
-                }
-            }
+    // Nhị phân trên k (cạnh s = k*edge) k * edge <= min(n, m)
+    int loK = 0, hiK = min(n, m) / edge, ans = 0;
+    while (loK <= hiK) {
+        int midK = (loK + hiK) / 2;
+        int s = midK * edge;
+        if (ok(s, n, m, e, t)) {
+            ans = s; 
+            loK = midK + 1; 
+        } else {
+            hiK = midK - 1; 
         }
     }
-    cout<<res;
+    cout << ans;
 }
