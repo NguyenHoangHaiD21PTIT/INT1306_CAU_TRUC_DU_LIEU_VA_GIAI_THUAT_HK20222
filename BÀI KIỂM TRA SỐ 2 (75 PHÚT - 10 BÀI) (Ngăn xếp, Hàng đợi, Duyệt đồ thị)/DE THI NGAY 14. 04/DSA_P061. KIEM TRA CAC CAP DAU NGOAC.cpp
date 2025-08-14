9a1e829cb699c101d64fe9504d7972c37a1e44ca
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-14 08:54:53 +0700

    Update DSA_P061. KIEM TRA CAC CAP DAU NGOAC.cpp

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 14. 04/DSA_P061. KIEM TRA CAC CAP DAU NGOAC.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 14. 04/DSA_P061. KIEM TRA CAC CAP DAU NGOAC.cpp
@@ -1,36 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 bool valid(char a, char b){ 
-	if((a=='('&&b==')')||(a=='['&&b==']') ||(a=='{'&&b=='}')) return true;
+	if((a == '(' && b == ')')|| (a == '[' && b == ']')|| (a == '{' && b == '}')) return true;
 	return false;
 }
-
 bool check(string s){
 	stack<char>st;
 	for(char i: s){
-		if(i=='('||i=='['||i=='{'){
+		if(i == '(' || i == '[' || i == '{'){
 			st.push(i);
 		} else { 
 			if(st.empty()) return false;
-			if(valid(st.top(),i)) st.pop(); 
+			if(valid(st.top(), i)) st.pop(); 
 			else return false; 
 		}
 	}
 	return st.empty();
 }
-
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
-		string s;
-		cin>>s;
-		if(check(s)) cout<<"true"<<endl;
-		else cout<<"false"<<endl;
+		string s; cin >> s;
+		if(check(s)) cout << "true" << endl;
+		else cout << "false" << endl;
 	}
 }
-/*
-2
-[()]{}{[()()]()}
-[(]) */
