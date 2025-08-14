18bac1d70d22c18ef9635ce18db9b193490bf0c5
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-14 08:41:41 +0700

    Update and rename DSA_P052. DANH SO THU TU CAP DAU NGOAC.cpp to DSA_P052. ĐÁNH SỐ THỨ TỰ CẶP DẤU NGOẶC.cpp

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 12. 04/DSA_P052. ĐÁNH SỐ THỨ TỰ CẶP DẤU NGOẶC.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 12. 04/DSA_P052. ĐÁNH SỐ THỨ TỰ CẶP DẤU NGOẶC.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	cin.ignore();
 	while(t--){
 		string s;
 		getline(cin, s);
 		int cnt = 1;
-		stack<int>st;
+		stack <int> st;
 		for(int i = 0;i<s.size();i++){
 			if(s[i] == '('){
 				st.push(cnt);
-				cout<<cnt<<" ";
-				cnt++;
+				cout << cnt++ << " ";
 			} else if (s[i] == ')'){
-				cout<<st.top()<<" ";
+				cout << st.top() << " ";
 				st.pop();
 			}
 		}
-		cout<<endl;
+		cout << endl;
 	}
 }
