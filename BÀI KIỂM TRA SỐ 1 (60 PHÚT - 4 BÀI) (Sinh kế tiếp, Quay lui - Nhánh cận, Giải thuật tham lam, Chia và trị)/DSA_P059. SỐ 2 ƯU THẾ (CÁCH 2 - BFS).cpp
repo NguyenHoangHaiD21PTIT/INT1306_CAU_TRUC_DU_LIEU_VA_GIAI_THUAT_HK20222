bf4bb3d8192f2e96c78076e6bc9c4649a13b10ca
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-17 22:04:41 +0700

    Format code

--- BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P059. SỐ 2 ƯU THẾ (CÁCH 2 - BFS).cpp
+++ BÀI KIỂM TRA SỐ 1 (60 PHÚT - 4 BÀI) (Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị)/DSA_P059. SỐ 2 ƯU THẾ (CÁCH 2 - BFS).cpp
@@ -1,26 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 string s = "012";
 vector<string>v;
-
 int check(string s){
-	map<char, int>mp;
+	map <char, int> mp;
 	for(auto i: s) mp[i]++;
-	if(mp['2']>s.size()/2) return true;
+	if(mp['2'] > s.size()/2) return true;
 	else return false;
 }
-
 void gen(){
-	//Buoc 1: Khoi tao
-	queue<string>q;
+	queue <string> q;
 	q.push("1"); q.push("2");
 	v.push_back("2");
-	//Buoc 2: Lap
 	while(1){
 		string x = q.front();
 		q.pop();
-		if(v.size()==1005) break;
+		if(v.size() == 1005) break;
 		for(char i: s){
 			string s1 = x + i;
 			q.push(s1);
@@ -28,15 +23,12 @@ void gen(){
 		}
 	}
 }
-
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	gen();
-	while(t--){
-		int n;
-		cin>>n;
-		for(int i = 0;i<n;i++) cout<<v[i]<<" ";
-		cout<<endl;
+	while(t --){
+		int n; cin >> n;
+		for(int i = 0; i < n; i++) cout << v[i] << " ";
+		cout << endl;
 	}
 }
