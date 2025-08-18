ebf62630be33cbf6c431c77656776081de91bf46
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-18 21:15:35 +0700

    Update explain

--- LUYEN TAP NANG CAO/1548. PHAN TU TRUNG VI.cpp
+++ LUYEN TAP NANG CAO/1548. PHAN TU TRUNG VI.cpp
@@ -5,14 +5,15 @@ const int MAXN = 1e6 + 5;
 int bit[MAXN]; 
 int cnt = 0; 
 
-void update(int idx, int val) {
+//BIT[x] lúc này lưu số phần tử của giá trị x
+void update(int idx, int val) { //BIT[x] += val
     while (idx < MAXN) {
         bit[idx] += val;
         idx += idx & -idx;
     }
 }
 
-int get(int idx) {
+int get(int idx) { //Tổng tần suất các phần tử từ đầu đến idx
     int sum = 0;
     while (idx > 0) {
         sum += bit[idx];
@@ -21,11 +22,14 @@ int get(int idx) {
     return sum;
 }
 
-int find_kth(int k) {
+int find_kth(int k) { //Tìm phần tử thứ k trong dãy số
     int l = 1, r = MAXN - 1, ans = -1;
     while (l <= r) {
         int mid = (l + r) / 2;
+        //Chú ý là phải lùi đến khi nào không lùi được nữa mới dừng, vì pos tìm được có thể là pos "rác" (Thực tế trong mảng không có)
+        //Bởi vì phần tử thứ k là vị trí mà ngay sau vị trí pos, với pos là vị trí cuối cùng tổng tần số < k
         if (get(mid) >= k) {
+            //get (mid): freq[0] + freq[1] + ... + freq[mid]
             ans = mid;
             r = mid - 1;
         } else {
@@ -36,19 +40,14 @@ int find_kth(int k) {
 }
 
 int main() {
-    int Q;
-    cin >> Q;
+    int Q; cin >> Q;
     while (Q--) {
-        string q;
-        cin >> q;
+        string q; cin >> q;
         if (q == "add") {
-            int x;
-            cin >> x;
-            update(x, 1);
-            cnt++;
+            int x; cin >> x;
+            update(x, 1); cnt++;
         } else if (q == "del") {
-            int x;
-            cin >> x;
+            int x; cin >> x;
             update(x, -1);
             cnt--;
         } else if (q == "print") {
