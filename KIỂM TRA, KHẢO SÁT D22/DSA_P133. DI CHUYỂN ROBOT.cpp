91b99ec31b7b0b4ed926b3ba104d03c9d7bf31ca
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-24 16:31:41 +0700

    Update DSA_P133. DI CHUYỂN ROBOT + DSAKT074. ROBOCON.cpp

--- KIỂM TRA, KHẢO SÁT D22/DSA_P133. DI CHUYỂN ROBOT.cpp
+++ KIỂM TRA, KHẢO SÁT D22/DSA_P133. DI CHUYỂN ROBOT.cpp
@@ -1,59 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    int i, j, tmod, dist;
-};
-//Màu ban đầu là gì, để sau tmod bước, đạt màu target: Ban đầu + t bước = Target --> Lấy mod cả 2 vế
-int need_c0(int target, int tmod) {
-    int x = target - tmod;
+struct St {
+    int u, v, t, d; 
+}; 
+// u: robot A tại đỉnh u
+// v: robot B tại đỉnh v
+// t: bước hiện tại mod 3
+// d: số bước đã đi
+
+// Hàm tính màu ban đầu của cạnh để sau t bước khớp màu đích
+int need(int goal, int t) {
+    int x = goal - t;
     x %= 3;
-    if (x <= 0) x += 3; 
+    if (x <= 0) x += 3;
     return x;
 }
+
 int main() {
     int T; cin >> T;
     while (T--) {
-        int N, M;
-        cin >> N >> M;
-        vector<int> color(N + 1);
-        for (int i = 1; i <= N; ++i) cin >> color[i];
-        // adj[u][c0] = Tập đỉnh kề v của đỉnh u với điều kiện cạnh u --> v chứa màu c0
+        int n, m; 
+        cin >> n >> m;
+        vector<int> col(n + 1);
+        for (int i = 1; i <= n; i++) cin >> col[i];
+        // adj[x][c] = {y1, y2, ...}: Cạnh x --> y1, x --> y2 ... có màu ban đầu là c
         vector<int> adj[205][4];
-        for (int e = 0; e < M; ++e) {
-            int u, v, c0;
-            cin >> u >> v >> c0;
-            adj[u][c0].push_back(v);
+        for (int i = 0; i < m; i++) {
+            int a, b, c; 
+            cin >> a >> b >> c;
+            adj[a][c].push_back(b);
         }
-        // visited[Số bước di chuyển][Vị trí robot 1][Vị trí robot 2]
+        // vis[t][u][v] = t: Số bước mod 3, u, v: Vị trí Robot 1, 2 tại thời điểm t
         char vis[3][205][205] = {};
-        queue<Node> q;
+        queue<St> q;
         vis[0][1][2] = 1;
-        q.push({1, 2, 0, 0}); //qe(tmod, pos1, pos2, dist)
+        q.push({1, 2, 0, 0}); // (A = 1, B = 2, t = 0, dist = 0)
         int ans = -1;
         while (!q.empty()) {
-            Node cur = q.front(); q.pop();
-            int i = cur.i, j = cur.j, tmod = cur.tmod, dist = cur.dist;
-            if (i == j) { 
-                ans = dist; 
+            auto cur = q.front(); q.pop();
+            int a = cur.u, b = cur.v, t = cur.t, d = cur.d;
+            if (a == b) { 
+                ans = d;
                 break; 
             }
-            int need1 = need_c0(color[j], tmod); // màu ban đầu cần ở cạnh đi từ i
-            int need2 = need_c0(color[i], tmod); // màu ban đầu cần ở cạnh đi từ j
-            int next_t = (tmod + 1) % 3;
-            const vector<int> U = adj[i][need1], V = adj[j][need2];
-            if (U.empty() || V.empty()) continue;
-            for (int ui = 0; ui < (int)U.size(); ++ui) {
-                int u = U[ui];
-                for (int vi = 0; vi < (int)V.size(); ++vi) {
-                    int v = V[vi];
-                    if (!vis[next_t][u][v]) {
-                        vis[next_t][u][v] = 1;
-                        q.push({u, v, next_t, dist + 1});
+            // Lúc này, robot A ở a, robot B ở b.
+            // Robot A muốn đi cạnh mà màu cạnh lúc t = màu đỉnh B.
+            // Vì đồ thị chỉ lưu màu cạnh ban đầu (t = 0), ta phải tính ra màu ban đầu tương ứng.
+            // Tức là: "Cạnh lúc đầu có màu gì để sau t bước màu cạnh = màu đỉnh B".
+            int c1 = need(col[b], t), c2 = need(col[a], t); 
+            int nt = (t + 1) % 3;
+            for (int x : adj[a][c1]) {
+                for (int y : adj[b][c2]) {
+                    if (!vis[nt][x][y]) {
+                        vis[nt][x][y] = 1;
+                        q.push({x, y, nt, d + 1});
                     }
                 }
             }
         }
-        cout << ans << '\n';
+        cout << ans << "\n";
     }
 }
