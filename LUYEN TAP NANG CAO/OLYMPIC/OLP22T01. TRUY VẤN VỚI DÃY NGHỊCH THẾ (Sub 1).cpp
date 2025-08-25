008bdcb454e8802f19a3bc7c322d4c8411c9ea20
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-25 09:25:52 +0700

    Update explain

--- LUYEN TAP NANG CAO/OLYMPIC/OLP22T01. TRUY VẤN VỚI DÃY NGHỊCH THẾ (Sub 1).cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP22T01. TRUY VẤN VỚI DÃY NGHỊCH THẾ (Sub 1).cpp
@@ -9,7 +9,12 @@ int main() {
     // Ví dụ: 5 2 3 3 0. Ban đầu, ngưỡng là 0. Tất cả bị ép xuống 0 --> 0 0 0 0 0
     // Ngưỡng là 1: 1 1 1 1 0
     // Ngưỡng là 3: 3 2 3 3 0
-    // Ví dụ: Nghịch thế 5, 2. Ngưỡng là 3, 4: 2 để im, 5 bị ép thành 3, 4. Như vậy, cặp 5, 2 được bật khi ngưỡng = 3 = Min + 1
+    // Ví dụ: Nghịch thế 5, 2. Quy tắc là không quá ngưỡng thì để nguyên, quá ngưỡng thì ép xuống bằng ngưỡng
+    // Ngưỡng là 0: Cả 2 bị ép về 0, 0. Ngưỡng là 1 cũng vậy. Ngưỡng là 2 thì 5 bị ép về 2: 2, 2. 
+    // Ngưỡng là 3: 2 để im, 5 bị ép thành 3. 3 > 2. Như vậy, cặp 5, 2 trở thành nghịch thế khi ngưỡng là 3 = 2 + 1
+    // Ngưỡng là 4: 2 để im, 5 bị ép thành 4. 4 > 2. Đây không phải cặp nghịch thế mới nữa
+    // Ngưỡng là >= 5: Cả 2 số để im. 5 > 2. Đây cũng là cặp nghịch thế cũ.
+    // Như vậy, 1 cặp nghịch thế (Max, Min) chỉ được bật lên 1 lần duy nhất tại ngưỡng Min + 1. 
     // Đến ngưỡng trên 5 thì 5 được để nguyên. Do đó, nghịch thế chỉ phát sinh duy nhất 1 lần khi ngưỡng từ 2 bật lên 3
     vector<long long> delta(N + 2, 0);
     for (int i = 0; i < N; ++i) {
@@ -26,3 +31,4 @@ int main() {
         cout << run << "\n";
     }
 }
+
