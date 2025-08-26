21667bb16d239ff3137c311b153ad4019fedf0e9
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-08-26 19:29:11 +0700

    Update DSA_P297. XAU TAM PHAN CHAN.cpp

--- THI CHINH THUC - 2024/DSA_P297. XAU TAM PHAN CHAN.cpp
+++ THI CHINH THUC - 2024/DSA_P297. XAU TAM PHAN CHAN.cpp
@@ -4,31 +4,30 @@ using namespace std;
 int n, ok, a[100];
 
 void kt(){
-    for(int i = 1;i<=n;i++) a[i] = 1;
+    for(int i = 1; i <= n; i++) a[i] = 1;
 }
 
 void sinh(){
     int i = n;
-    while(i>=1&&a[i]==3) i--;
-    if(i==0) ok = 0;
+    while(i >= 1 && a[i] == 3) i--;
+    if(i == 0) ok = 0;
     else{
         a[i]++;
-        for(int j = i + 1;j<=n;j++) a[j] = 1;
+        for(int j = i + 1; j <= n; j++) a[j] = 1;
     }
 }
 
 int main(){
-    cin>>n;
-    kt();
-    ok = 1;
+    cin >> n; kt(); ok = 1;
     int cnt = 1;
-    while(ok==1){
-        if(cnt%2==0){
-            cout<<cnt<<": ";
-            for(int i = 1;i<=n;i++) cout << a[i] - 1;
-            cout<<endl;
+    while(ok){
+        if(cnt % 2 == 0){
+            cout << cnt << ": ";
+            for(int i = 1; i <= n; i++) cout << a[i] - 1;
+            cout << endl;
         }
         cnt++;
         sinh();
     }
 }
+
