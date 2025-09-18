e07a3faa1df8df563fefa88f8b06584bfc4ac76f
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-09-18 18:03:09 +0700

    Lời giải nhanh nhất

--- CODE ICPC 2024/CODE CHUNG KET/OLP182 + PTIT 2024B. SỐ MAY MẮN ĐẶC BIỆT.cpp
+++ CODE ICPC 2024/CODE CHUNG KET/OLP182 + PTIT 2024B. SỐ MAY MẮN ĐẶC BIỆT.cpp
@@ -1,58 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct St { 
-    int x, y; 
-};
-
-int rmd[205][205];   // rmd[x][y] = dư số S(x,y) mod n
-
-// In số S(x,y) = 88...866...6
-void pr(int x, int y) {
-    for (int i = 0; i < x; i++) cout << '8';
-    for (int i = 0; i < y; i++) cout << '6';
-    cout << "\n";
-}
-
-// (10^k) mod n
-int pw(int k, int n) {
-    int res = 1;
-    for (int i = 0; i < k; i++) res = res * 10 % n;
-    return res;
-}
-
-void solve(int n) {
-    for (int i = 0; i <= 200; i++) {
-        for (int j = 0; j <= 200; j++) rmd[i][j] = -1;
-    }
-    queue<St> q;
-    q.push({0, 0});
-    rmd[0][0] = 0;
-    while (!q.empty()) {
-        auto cur = q.front(); q.pop();
-        int x = cur.x, y = cur.y;
-        if (x + y > 0 && rmd[x][y] == 0) {
-            pr(x, y);
-            return;
-        }
-        //Thêm 6 vào cuối: Nhân thêm 10 để lấy chỗ cho chữ số cuối cùng, cộng tương ứng chữ số ấy vào
-        if (y + 1 <= 200 && rmd[x][y + 1] == -1) {
-            rmd[x][y + 1] = (rmd[x][y] * 10 + 6) % n;
-            q.push({x, y + 1});
-        }
-        //Thêm 8 vào đầu. Ví dụ: Số 12345 có 5 chữ số, thêm 8 vào đầu: 812345 = 8 * 10^(5 = Số chữ số số gốc) + 12345(Số gốc)
-        if (x + 1 <= 200 && rmd[x + 1][y] == -1) {
-            rmd[x + 1][y] = ((8 * pw(x + y, n)) + rmd[x][y]) % n;
-            q.push({x + 1, y});
+string sol(int m) {
+    const int L = 200;
+    vector<int> pw(L + 1, 1), one(L + 1, 0);//pw[i] = (10^i) % m, one[i] = (11...1 -> i số 1) % m
+    for (int i = 1; i <= L; i++) pw[i] = pw[i - 1] * 10 % m;
+    for (int i = 1; i <= L; i++) one[i] = (one[i - 1] * 10 + 1) % m;
+    for (int len = 1; len <= L; len++) { //Độ dài xâu
+        for (int a = 0; a <= len; a++) { //Số chữ số 8 = a
+            int b = len - a, A = 8 * one[a] % m, B = 6 * one[b] % m;//A = (88 ... 8 -> i số 8) % m, B = (i số 6) % m
+            //Muốn ghép a số 8 và b số 6 thành số, ta phải dịch tráo a số 8 sang 10^b để nhường chỗ cho b số 6
+            if ((1LL * A * pw[b] + B) % m == 0) return string(a, '8') + string(b, '6');
         }
     }
-    cout << "-1\n";
+    return "-1";
 }
 
 int main() {
-    int T, n; cin >> T;
-    while (T--) {
-        cin >> n; solve(n);
+    int t, m; cin >> t;
+    while (t--) {
+        cin >> m;
+        cout << sol(m) << '\n';
     }
 }
 //Link submit: https://oj.vnoi.info/problem/luckynum
