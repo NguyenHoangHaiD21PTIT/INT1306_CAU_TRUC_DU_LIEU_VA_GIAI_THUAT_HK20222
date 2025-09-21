974f0341ced66e064a8b3da495fce596efd4b757
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-09-21 09:14:52 +0700

    Update BAI 1. THE LUC VA TRI TUE.cpp

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/THI CHUNG CHI 2024/BAI 1. THE LUC VA TRI TUE.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/THI CHUNG CHI 2024/BAI 1. THE LUC VA TRI TUE.cpp
@@ -1,11 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    long long a, b, c, d;
-    cin>>a>>b>>c>>d;
-    if(c > d) swap(c, d);
-    long long tongchan = 0, tongle = 0;
-    for(int i = a;i<=b;i+=2) tongchan+=i;
-    for(int i = c;i<=d;i+=2) tongle+=i;
-    cout<<abs(tongchan - tongle) * 2;
+#define ll long long
+int main() {
+    ll a, b, c, d; 
+    cin >> a >> b >> c >> d;
+    if (c > d) swap(c, d);
+    ll chan = ((b - a) / 2 + 1) * (a + b) / 2;  
+    ll le   = ((d - c) / 2 + 1) * (c + d) / 2;  
+    cout << 2 * llabs(chan - le);
 }
