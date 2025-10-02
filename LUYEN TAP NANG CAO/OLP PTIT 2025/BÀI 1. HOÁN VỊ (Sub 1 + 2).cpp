cfedf4a99eb1100a330a34500aa90c362e0e2744
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-10-02 18:16:21 +0700

    Update BÀI 1. HOÁN VỊ (Sub 1 + 2).cpp

--- LUYEN TAP NANG CAO/OLP PTIT 2025/BÀI 1. HOÁN VỊ (Sub 1 + 2).cpp
+++ LUYEN TAP NANG CAO/OLP PTIT 2025/BÀI 1. HOÁN VỊ (Sub 1 + 2).cpp
@@ -1,5 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout << "I am PTITer";
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> P(n);
+    for (int i = 0; i < n; i++) cin >> P[i];
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+        vector<int> Q = P;
+        swap(Q[x], Q[y]);
+        vector<bool> vis(n, false);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                cnt++;
+                int cur = i;
+                while (!vis[cur]) {
+                    vis[cur] = true;
+                    cur = Q[cur];
+                }
+            }
+        }
+        cout << cnt << "\n";
+    }
 }
