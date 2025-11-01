84d0351800f9a8b6ac1d3693e2dc32db3ec350c4
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-01 15:17:48 +0700

    Rename DSA_P017 file to include correct title

---
+++ BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P017. SỐ LỘC PHÁT.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, a[100], ok;
+
+void sinh(){
+	int i = n - 1;
+	while(i >= 0 && a[i] == 8){
+		a[i] = 6;
+		i--;
+	}
+	if(i == -1) ok = 0;
+	else a[i] = 8;
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		cin >> n; ok = 1;
+		for(int i = 0; i < n; i++) a[i] = 6;
+		cout << pow(2, n) << endl;
+		while(ok==1){
+			for(int i = 0; i < n; i++) cout << a[i];
+			cout << " "; sinh();
+		}
+		cout << endl;
+	} 
+}
