fc692a8fc567df6eff778e1826bbb1baafd8f4f1
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-01 18:59:15 +0700

    Bài 2 sub 1

---
+++ LUYEN TAP NANG CAO/Luyện đề 2025/CONTEST THẦY HOÀNG/OLP257. CHUYỂN CHỮ SỐ (DGMOVE) (Subtask 1).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a; cin >> a;
+    string s = to_string(a);
+    int n = s.length();
+    string res = s;
+    for (int i = 0; i < n; i++) {
+        char d = s[i];
+        string tmp = s;
+        tmp.erase(i, 1); // Xóa 1 ký tự tại vị trí i
+        string sf = d + tmp; res = max(res, sf);
+        string sb = tmp + d; res = max(res, sb);
+    }
+    cout << res << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T; cin >> T;
+    while (T--) solve();
+}
