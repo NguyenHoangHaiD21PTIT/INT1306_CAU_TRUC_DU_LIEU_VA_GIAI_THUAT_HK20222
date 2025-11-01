26ad8d1e8d994189b14473ec57324b60293dba33
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-01 15:22:21 +0700

    Refactor DSA_P003. MA SO.cpp with comments
    The code now includes comments to explain the purpose of the 'gen' function and improves readability.

--- DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P003. MA SO.cpp
+++ DE LUYEN TAP TUAN 1/CODE THAM KHẢO/DSA_P003. MA SO.cpp
@@ -2,39 +2,36 @@
 using namespace std;
 
 int n; vector<string>v2;
-
-void gen(){
+void gen(){ //Sinh các tổ hợp số 11, 12, 21, 22, ...
 	queue<string>q;
-	for(int i = 1;i<=n;i++){
-	 	string tmp="";
-		tmp+=to_string(i);
+	for(int i = 1;i <= n;i++){
+	 	string tmp = "";
+		tmp += to_string(i);
 		q.push(tmp);
 	}
 	while(1){
 		string x = q.front();
 		q.pop();
-		if(x.size()==n) break;
-		for(int i = 1;i<=n;i++){
+		if(x.size() == n) break;
+		for(int i = 1;i <= n;i++){
 			string x1 = x + to_string(i);
 			q.push(x1);
-			if(x1.size()==n)  v2.push_back(x1);
+			if(x1.size() == n)  v2.push_back(x1);
 		}
 	}
 }
 
 int main(){
-	cin>>n;
-	gen();
-	char c='A';
-	string s1="A";
-	for(int i=1;i<n;i++){
+	cin >> n; gen();
+	char c = 'A'; string s1 = "A";
+	for(int i = 1; i < n; i++){
 		c++;
-		s1=s1+c;
+		s1 += c;
 	}
-	vector<string>v1;
+	vector <string> v1;
 	v1.push_back(s1);
-	while(next_permutation(s1.begin(),s1.end())!=false) v1.push_back(s1);
+	while(next_permutation(s1.begin(),s1.end())) v1.push_back(s1);
 	for(auto i: v1){
-		for(auto j: v2) cout<<i<<j<<endl;
+		for(auto j: v2) cout << i << j << endl;
 	} 
 }
