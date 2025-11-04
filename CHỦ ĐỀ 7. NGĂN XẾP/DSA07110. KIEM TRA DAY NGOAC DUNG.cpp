0420c1a1ecdb59fea2a3777f49bde74a7719b030
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-04 18:41:33 +0700

    Refactor bracket validation logic and comments
    Refactor the code to improve readability and comments.

--- CHỦ ĐỀ 7. NGĂN XẾP/DSA07110. KIEM TRA DAY NGOAC DUNG.cpp
+++ CHỦ ĐỀ 7. NGĂN XẾP/DSA07110. KIEM TRA DAY NGOAC DUNG.cpp
@@ -2,32 +2,30 @@
 using namespace std;
 
 bool valid(char a, char b){ //Cap dong mo tuong ung
-	if((a=='('&&b==')')||(a=='['&&b==']') ||(a=='{'&&b=='}')) return true;
+	if((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}')) return true;
 	return false;
 }
 
 bool check(string s){
 	stack<char>st;
 	for(char i: s){
-		if(i=='('||i=='['||i=='{'){
+		if(i == '(' || i == '[' || i == '{'){ //Gặp dấu mở ngoặc thì cho vào stack
 			st.push(i);
-		} else { //Gap dau mo ngoac
-			if(st.empty()) return false;//Khong co dau dong
-			if(valid(st.top(),i)) st.pop(); //Cung loai --> Cho ra ngoai
-			else return false; //Co dau dong nhung khong cung loai
+		} else { //Gặp dấu đóng ngoặc
+			if(st.empty()) return false;//Không có dấu mở ngoặc tương ứng
+			if(valid(st.top(),i)) st.pop(); //Có dấu mở ngoặc cùng loại --> Cho cặp này ra ngoài
+			else return false; //Có dấu đóng ngoặc nhưng khác loại
 		}
 	}
-	return st.empty();
+	return st.empty(); //Stack không rỗng chứng tỏ có dấu mở nhưng không có dấu dóng để ghép
 }
 
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
-		string s;
-		cin>>s;
-		if(check(s)) cout<<"YES"<<endl;
-		else cout<<"NO"<<endl;
+		string s; cin >> s;
+		if(check(s)) cout << "YES" << endl;
+		else cout << "NO" << endl;
 	}
 }
 /*
