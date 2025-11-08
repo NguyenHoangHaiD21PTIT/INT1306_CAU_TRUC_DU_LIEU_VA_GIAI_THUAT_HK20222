cdc15202b5bb2b72dfa6332fd0c2a1b207c20504
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-08 09:28:42 +0700

    Refactor Fibonacci function and variable types

--- CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04021 - DAY XAU NHI PHAN.cpp
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04021 - DAY XAU NHI PHAN.cpp
@@ -1,31 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-long long f[100];
+ll f[100];
 
-void fibo(){
-	f[1] = 1;
-	f[2] = 1;
-	for(long long i=3;i<=92;i++) f[i] = f[i-2] + f[i-1];
+void pre(){
+	f[1] = 1; f[2] = 1;
+	for(int i = 3; i <= 92; i++) f[i] = f[i - 2] + f[i - 1];
 }
 
-char find(long long n, long long k){
-	if(n==1) return '0';
-	if(n==2) return '1';
-	if(k<=f[n-2]){
-		return find(n-2,k);
-	} else {
-		return find(n-1,k-f[n-2]);
-	}
+char find(int n, ll k){
+	if(n == 1) return '0';
+	if(n == 2) return '1';
+	if(k <= f[n - 2]) return find(n - 2,k);
+	else return find(n - 1, k - f[n - 2]);
 }
 
 int main(){
-	int t;
-	cin>>t;
-	fibo();
+	int t; cin >> t;
+	pre();
 	while(t--){
-		long long n, k;
-		cin>>n>>k;
-		cout<<find(n,k)<<endl;
+		int n; ll k;
+		cin >> n >> k;
+		cout << find(n, k) << endl;
 	}
 }
