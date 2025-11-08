6edadf9049db50ecf8f70ab12db55f471343bb7e
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-08 10:09:13 +0700

    Refactor power function and main loop structure

--- CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04035 - LUY THUA.cpp
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04035 - LUY THUA.cpp
@@ -1,22 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int MOD = 1e9+7;
-long long powdu(long long a, long long b){
-	long long res=1;
-	while(b){
-		if(b%2==1){
-			res=(res*a)%MOD;
-		}
-		a=(a*a)%MOD;
-		b/=2;
-	}
-	return res;
+using ll = long long;
+const int MOD = 1e9 + 7;
+ll lt(ll a, ll b){
+    ll res = 1;
+    while (b){
+        if (b % 2 == 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b /= 2;
+    }
+    return res;
 }
 int main(){
-	while(1){
-		long long a, b;
-		cin>>a>>b;
-		if(a==0&&b==0) break;
-		cout<<powdu(a,b)<<endl;
-	}
+    ll a, b;
+    while (cin >> a >> b && (a != 0 || b != 0)) cout << lt(a, b) << endl;
 }
