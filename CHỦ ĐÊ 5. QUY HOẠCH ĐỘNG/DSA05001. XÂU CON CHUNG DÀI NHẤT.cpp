32dc5d74df3d47d841c47ee878c705007c4634a3
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-11 20:44:41 +0700

    Update and rename DSA05001. XAU CON CHUNG DAI NHAT.cpp to DSA05001. XÂU CON CHUNG DÀI NHẤT.cpp

---
+++ CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSA05001. XÂU CON CHUNG DÀI NHẤT.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[1005][1005] = {}; //dp[i][j]: độ dài lớn nhất của xâu con chung dài nhất của s1[0..i-1] và s2[0..j-1]
+
+int solve(string s1, string s2){
+	memset(dp, 0, sizeof(dp));
+	for(int i = 1;i <= s1.size(); i++){
+		for(int j = 1; j <= s2.size(); j++){
+			if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+			else dp[i][j] = max (dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	return dp[s1.size()][s2.size()];
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		string s1, s2;
+		cin >> s1 >> s2;
+		cout << solve(s1, s2) << endl;
+	}
+}
+/*
+2
+AGGTAB
+GXTXAYB
+AA
+BB */
