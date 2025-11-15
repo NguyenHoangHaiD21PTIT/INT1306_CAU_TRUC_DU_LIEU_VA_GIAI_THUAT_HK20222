7615aa71ae41dd65448536f6520fa62d34889212
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-15 15:47:52 +0700

    Update DSA07031. TIEN TO.cpp

--- CHỦ ĐỀ 7. NGĂN XẾP/DSA07031. TIEN TO.cpp
+++ CHỦ ĐỀ 7. NGĂN XẾP/DSA07031. TIEN TO.cpp
@@ -1,50 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-long long calc(long long x, long long y, string s){
-	if(s=="+") return x + y;
-	else if (s=="-") return x - y;
-	else if (s=="*") return x * y;
-	else return x/y;
+ll calc(ll x, ll y, string s){
+	if(s == "+") return x + y;
+	else if (s == "-") return x - y;
+	else if (s == "*") return x * y;
+	else return x  /y;
 }
 
 int toantu(string s){
-	if(s=="+"||s=="-"||s=="*"||s=="/") return 1;
+	if(s == "+" ||s == "-" ||s == "*" ||s == "/") return 1;
 	else return 0;
 }
 
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
-		int n;
-		cin>>n;
-		vector<string>v;
+		int n; cin >> n;
+		vector <string> v;
 		while(n--){
-			string s;
-			cin>>s;
+			string s; cin >> s;
 			v.push_back(s);
 		}
-		stack<long long>st;
-		for(int i = v.size()-1;i>=0;i--){
-			if(!toantu(v[i])){
-				long long x = stoll(v[i]);
-				st.push(x);
-			} else {
-				long long x1 = st.top(); st.pop();
-				long long x2 = st.top(); st.pop();
-				long long x3 = calc(x1, x2, v[i]);
-				st.push(x3);
+		stack<ll>st;
+		for(int i = v.size() - 1; i >= 0; i--){
+			if(!toantu(v[i])) st.push(stoll(v[i]));
+			else {
+				ll x1 = st.top(); st.pop();
+				ll x2 = st.top(); st.pop();
+				st.push(calc(x1, x2, v[i]));
 			}
 		}
-		cout<<st.top()<<endl;
+		cout << st.top() << endl;
 	}
 }
-/*
-3
-7
-- + 2 * 3 1 9
-7
-- + 8 * 7 5 9
-3
-+ -10 -4 */
