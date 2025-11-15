2e2d2e7e6afcc051795e1ee62ff50c79cdbbae4f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-15 09:05:07 +0700

    Update and rename DSA09025 - DUONG DI THEO DFS VOI DO THI CO HUONG.cpp to DSA09025. ĐƯỜNG ĐI THEO DFS VỚI ĐỒ THỊ CÓ HƯỚNG.cpp

---
+++ CHỦ ĐỀ 9. DUYỆT ĐỒ THỊ/DSA09025. ĐƯỜNG ĐI THEO DFS VỚI ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> adj[1005];
+int vis[1005] = {}, par[1005] = {};
+
+void DFS(int x){
+	vis[x] = 1;
+	for(int i: adj[x]){
+		if(!vis[i]){
+			DFS(i);
+			par[i] = x;
+		}
+	}
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		memset(vis, 0, sizeof(vis));
+		memset(par, 0, sizeof(par));
+		int v, e, x, y;
+		cin >> v >> e >> x >> y;
+		for(int i = 1; i <= v; i++) adj[i].clear();
+		while(e--){
+			int a, b;
+			cin >> a >> b;
+			adj[a].push_back(b);
+		}
+		DFS(x);
+		if(!vis[y]) cout << -1;
+		else {
+			vector <int> path;
+			while(y != x){
+				path.push_back(y);
+				y = par[y];
+			}
+			path.push_back(x);
+			for(int i = path.size() - 1; i >= 0; i--) cout << path[i] << " ";
+		}
+		cout << endl;
+	}
+}
