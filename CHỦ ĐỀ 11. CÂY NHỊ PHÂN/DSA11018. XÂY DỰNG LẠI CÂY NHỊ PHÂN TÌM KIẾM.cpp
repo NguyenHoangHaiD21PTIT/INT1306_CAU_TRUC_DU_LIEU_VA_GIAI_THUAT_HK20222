b65923c9ff39889a7fc8bc96d0cbdc2f0a3122dd
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-18 19:01:22 +0700

    Rename file for consistency in naming

--- CHỦ ĐỀ 11. CÂY NHỊ PHÂN/DSA11018. XÂY DỰNG LẠI CÂY NHỊ PHÂN TÌM KIẾM.cpp
+++ CHỦ ĐỀ 11. CÂY NHỊ PHÂN/DSA11018. XÂY DỰNG LẠI CÂY NHỊ PHÂN TÌM KIẾM.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 struct node{
 	int data;
-	node *left;
-	node *right;
+	node *left, *right;
 	node(int x){
 		data = x;
 		left = right = NULL;
@@ -13,33 +12,31 @@ struct node{
 
 void preorder(node *root){
 	if(root == NULL) return;
-	cout<<root->data <<" ";	
+	cout << root -> data << " ";	
 	preorder(root -> left);
 	preorder(root -> right);
 	
 }
 
 void insert(node* &root, int x) {
-	if(root==NULL){
+	if(root == NULL){
 		root = new node(x);
 		return;
 	}
-	if(x<root->data) insert(root->left, x);
-	else insert(root->right, x);
+	if(x < root -> data) insert(root -> left, x);
+	else insert(root -> right, x);
 }
     
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
-		int n;
-		cin>>n;
+		int n; cin >> n;
 		node *root = NULL;
-		for(int i = 0;i<n;i++){
-			int x; cin>>x;
+		for(int i = 0;i < n;i++){
+			int x; cin >> x;
 			insert(root, x);
 		}
 		preorder(root);
-		cout<<endl;
+		cout << endl;
 	}
 }
