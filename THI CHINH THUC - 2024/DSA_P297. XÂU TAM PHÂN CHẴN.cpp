89ed44d1f8b48c5f962601bc5153ab4b768def1a
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-22 11:19:22 +0700

    Update and rename DSA_P297. XAU TAM PHAN CHAN.cpp to DSA_P297. XÂU TAM PHÂN CHẴN.cpp

--- THI CHINH THUC - 2024/DSA_P297. XÂU TAM PHÂN CHẴN.cpp
+++ THI CHINH THUC - 2024/DSA_P297. XÂU TAM PHÂN CHẴN.cpp
@@ -3,31 +3,27 @@ using namespace std;
 
 int n, ok, a[100];
 
-void kt(){
-    for(int i = 1; i <= n; i++) a[i] = 1;
-}
-
 void sinh(){
     int i = n;
-    while(i >= 1 && a[i] == 3) i--;
+    while(i >= 1 && a[i] == 2) i--;
     if(i == 0) ok = 0;
     else{
         a[i]++;
-        for(int j = i + 1; j <= n; j++) a[j] = 1;
+        for(int j = i + 1; j <= n; j++) a[j] = 0;
     }
 }
 
 int main(){
-    cin >> n; kt(); ok = 1;
+    cin >> n; ok = 1;
+    for(int i = 1; i <= n; i++) a[i] = 0;
     int cnt = 1;
     while(ok){
         if(cnt % 2 == 0){
             cout << cnt << ": ";
-            for(int i = 1; i <= n; i++) cout << a[i] - 1;
+            for(int i = 1; i <= n; i++) cout << a[i];
             cout << endl;
         }
         cnt++;
         sinh();
     }
 }
-
