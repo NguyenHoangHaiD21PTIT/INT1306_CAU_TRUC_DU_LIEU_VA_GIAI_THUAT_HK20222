06bf192ee9b832f8e40cbdf4ea81c10ac0bc85a4
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-22 11:23:52 +0700

    Rename file for consistency in naming

--- BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P002. LIỆT KÊ HOÁN VỊ - 1.cpp
+++ BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P002. LIỆT KÊ HOÁN VỊ - 1.cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int n, a[100], ok; cin >> n; 
-	for(int i = 0;i < n; i++) a[i] = i+1;
-	int cnt = 1;
-	cout << "1: ";
-	for(int i = 0;i < n; i++) cout << a[i] << " ";
+	int n, a[100], ok, cnt = 1; cin >> n; 
+	for(int i = 0;i < n; i++) a[i] = i + 1;
+	cout << cnt++ << ": ";
+	for(int i = 0; i < n; i++) cout << a[i] << " ";
 	cout << endl;
 	while(next_permutation(a, a + n)){
-		cnt++;
-		cout << cnt << ": ";
+		cout << cnt++ << ": ";
 		for(int i = 0;i < n; i++) cout << a[i] << " ";
 		cout << endl;
 	}
