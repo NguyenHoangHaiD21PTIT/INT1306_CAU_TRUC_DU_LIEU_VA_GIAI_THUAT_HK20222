97eb68ba24341e4b050788ba846db681f7e4434e
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-23 11:28:43 +0700

    Update and rename DSA05005. SO BUOC IT NHAT (LIS).cpp to DSA05005. SỐ BƯỚC ÍT NHẤT.cpp

---
+++ CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSA05005. SỐ BƯỚC ÍT NHẤT.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		int a[n], f[n];
+		for(int i = 0; i < n; i++){ 
+			cin >> a[i]; 
+			f[i] = 1; 
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < i; j++){
+				if(a[j] <= a[i]) f[i] = max(f[i], f[j] + 1);
+			}
+		}
+		cout << n - *max_element(f, f + n);
+	}
+}
