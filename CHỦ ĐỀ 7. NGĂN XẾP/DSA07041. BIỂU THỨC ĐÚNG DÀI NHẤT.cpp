51c1fe1d10742c614e3a9643e1b17620d8a63e0c
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-23 11:15:06 +0700

    Add files via upload

--- CHỦ ĐỀ 7. NGĂN XẾP/DSA07041. BIỂU THỨC ĐÚNG DÀI NHẤT.cpp
+++ CHỦ ĐỀ 7. NGĂN XẾP/DSA07041. BIỂU THỨC ĐÚNG DÀI NHẤT.cpp
@@ -1,1 +1,18 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while (t--){
+        string s; cin >> s;
+        int cnt = 0;
+        while(true){
+            int pos = s.find("()");
+            if (pos == -1) break;
+            else {
+                s.erase(pos, 2);
+                cnt+=2;
+            }
+        }
+        cout << cnt << "\n";
+    }
+}
