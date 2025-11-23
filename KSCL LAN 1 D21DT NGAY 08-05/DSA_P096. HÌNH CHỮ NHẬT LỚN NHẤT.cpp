d09443c521844e8b172a062884ae7ee2527680f6
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-11-23 11:00:34 +0700

    Update and rename DSA_P096. HINH CHU NHAT LON NHAT.cpp to DSA_P096. HÌNH CHỮ NHẬT LỚN NHẤT.cpp

--- KSCL LAN 1 D21DT NGAY 08-05/DSA_P096. HÌNH CHỮ NHẬT LỚN NHẤT.cpp
+++ KSCL LAN 1 D21DT NGAY 08-05/DSA_P096. HÌNH CHỮ NHẬT LỚN NHẤT.cpp
@@ -2,37 +2,30 @@
 using namespace std;
 
 int main(){
-    int t;
-    cin >> t;
+    int t; cin >> t;
     while(t--){
-        int n, m;
-        cin >> n >> m;
+        int n, m; cin >> n >> m;
         int a[n][m];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++)  cin >> a[i][j];
         }
-
-        // Tính chiều cao của các cột chỉ khi có số 1
+        // h[i][j]: Từ ô (i,j) đếm lên trên có bao nhiêu ô 1 liên tiếp
         int h[n][m];
-        memset(h, 0, sizeof(h)); // Gán tất cả các phần tử trong h bằng 0
+        memset(h, 0, sizeof(h)); 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(i==0) h[i][j] = a[i][j];
+                if(!i) h[i][j] = a[i][j];
                 else{
-                    if(a[i][j]==1) h[i][j] = h[i - 1][j] + 1;
+                    if(a[i][j]) h[i][j] = h[i - 1][j] + 1;
                     else h[i][j] = 0;
                 }
             }
         }
-
-        // Tính diện tích lớn nhất của hình chữ nhật trên mỗi hàng chỉ xét các cột có chiều cao khác 0
         int ans = 0;
         vector<pair<int, int>>vp;
         for(int i = 0; i < n; i++){
-            //Thực hiện bài toán HCN lớn nhất trên mỗi hàng
-            //Như vậy, mỗi hàng ta sẽ chỉ xét các cột
-            int l[m], r[m];// Tìm Vị trí phần tử đầu tiên bên trái, bên phải a[i] bé hơn a[i]
-            //l, r là cận trái, phải của HCN
+            // Thực hiện bài toán Histogram trên mảng 1D. Mỗi hàng là một mảng 1D.
+            int l[m], r[m];
             stack<int> st1, st2;
             for(int j = m - 1; j >= 0; j--){
                 while(!st1.empty() && h[i][j] <= h[i][st1.top()]) st1.pop();
@@ -46,26 +39,23 @@ int main(){
                 else l[j] = st2.top() + 1;
                 st2.push(j);
             }
-
             int res = 0;
             for(int j = 0; j < m; j++){
                 res = max(res, (r[j] - l[j] + 1) * h[i][j]);
-                vp.push_back({h[i][j],r[j] - l[j] + 1});
+                vp.push_back({h[i][j], r[j] - l[j] + 1});
             }
-            //Sau khi có HCN lớn nhất xét theo các cột chân chạm được đến hàng xét theo mỗi hàng, ta update
             ans = max(ans, res);
         }
-        pair<int, int>res;
-        int chenhlech = 1e9;
+        pair <int, int>res;
+        int dif = 1e9;
         for(auto x: vp){
             int S = x.first * x.second;
-            if(S==ans&&abs(x.first - x.second)<chenhlech){
-                chenhlech = abs(x.first - x.second);
+            if(S == ans && abs(x.first - x.second) < dif){
+                dif = abs(x.first - x.second);
                 res = x;
             }
         }
         if(res.first > res.second) swap(res.first, res.second);
-        cout<<res.first<<" "<<res.second<<endl;
+        cout << res.first << " " << res.second << endl;
     }
-    return 0;
 }
