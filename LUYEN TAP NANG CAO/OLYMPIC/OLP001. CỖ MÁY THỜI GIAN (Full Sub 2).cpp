6948f205f0f5e1050392b12f549d064e4d1c3afe
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-12-04 22:27:23 +0700

    Create OLP001. CỖ MÁY THỜI GIAN (Full Sub 2).cpp

---
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP001. CỖ MÁY THỜI GIAN (Full Sub 2).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll INF = 1e18;
+
+int n, q;
+vector<int> a; vector<ll> f; // f[i]: chi phí nhỏ nhất để đến vị trí i
+
+void solve() {
+    int s, t, k;
+    cin >> s >> t >> k;
+    s--; t--; 
+    if (s == t) { 
+        cout << a[s] << "\n"; 
+        return; 
+    }
+    if (s > t) swap(s, t);
+    //Bước 1: Khởi tạo trạng thái đầu
+    deque<int> dq; 
+    f[s] = a[s]; dq.push_back(s);
+    //Bước 2: Dùng dq để tìm chi phí nhỏ nhất
+    for (int i = s + 1; i <= t; ++i) {
+        while (!dq.empty() && dq.front() < i - k) dq.pop_front();//Xoá các trạng thái không còn khả dụng
+        f[i] = f[dq.front()] + a[i];//Cập nhật chi phí nhỏ nhất để đến vị trí i
+        while (!dq.empty() && f[dq.back()] >= f[i]) dq.pop_back();//Cập nhật dq
+        dq.push_back(i);
+    }
+    cout << f[t] << "\n";
+}
+
+int main() {
+    string s; cin >> n >> q >> s;
+    a.resize(n); f.resize(n);
+    for(int i = 0; i < n; ++i) a[i] = s[i] - '0';
+    while(q--) solve();
+}
