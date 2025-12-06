71b26e4974073269066f3b19a15b329d4f7c0424
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-12-06 21:30:36 +0700

    .

--- LUYEN TAP NANG CAO/OLYMPIC/OLP001. CỖ MÁY THỜI GIAN (Sub 3).cpp
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP001. CỖ MÁY THỜI GIAN (Sub 3).cpp
@@ -18,10 +18,10 @@ void run() {
     if (u > v) swap(u, v);
     L = 1; R = 0;
     f[u] = a[u];
-    q[++R] = u;
+    q[++R] = u; //Push back (Mở thêm 1 vị trí ở cuối, rồi cho nó vào)
     for (int i = u + 1; i <= v; ++i) {
         while (L <= R && q[L] < i - k) L++;     // Pop front
-        f[i] = f[q[L]] + a[i];                  //
+        f[i] = f[q[L]] + a[i];                  // q[L] = dq.front()
         while (L <= R && f[q[R]] >= f[i]) R--;  // Pop back
         q[++R] = i;                             // Push back
     }
@@ -29,7 +29,7 @@ void run() {
 }
 
 int main() {
-	  ios_base::sync_with_stdio(0); cin.tie(0);    string s;
+	ios_base::sync_with_stdio(0); cin.tie(0);    string s;
     cin >> n >> Q >> s;
     for (int i = 0; i < n; ++i) a[i] = s[i] - '0';
     while (Q--) run();
