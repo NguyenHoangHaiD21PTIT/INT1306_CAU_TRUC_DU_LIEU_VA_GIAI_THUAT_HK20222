728b909f3c6007322ec87b0201abd5b71e607c87
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-12-06 21:08:18 +0700

    Create OLP001. CỖ MÁY THỜI GIAN (Sub 3).cpp

---
+++ LUYEN TAP NANG CAO/OLYMPIC/OLP001. CỖ MÁY THỜI GIAN (Sub 3).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int N = 1005;
+
+int n, Q, a[N], q[N], L, R;
+ll f[N];
+
+void run() {
+    int u, v, k;
+    cin >> u >> v >> k;
+    u--; v--; 
+    if (u == v) { 
+        cout << a[u] << "\n"; 
+        return; 
+    }
+    if (u > v) swap(u, v);
+    L = 1; R = 0;
+    f[u] = a[u];
+    q[++R] = u;
+    for (int i = u + 1; i <= v; ++i) {
+        while (L <= R && q[L] < i - k) L++;     // Pop front
+        f[i] = f[q[L]] + a[i];                  //
+        while (L <= R && f[q[R]] >= f[i]) R--;  // Pop back
+        q[++R] = i;                             // Push back
+    }
+    cout << f[v] << "\n";
+}
+
+int main() {
+	  ios_base::sync_with_stdio(0); cin.tie(0);    string s;
+    cin >> n >> Q >> s;
+    for (int i = 0; i < n; ++i) a[i] = s[i] - '0';
+    while (Q--) run();
+}
