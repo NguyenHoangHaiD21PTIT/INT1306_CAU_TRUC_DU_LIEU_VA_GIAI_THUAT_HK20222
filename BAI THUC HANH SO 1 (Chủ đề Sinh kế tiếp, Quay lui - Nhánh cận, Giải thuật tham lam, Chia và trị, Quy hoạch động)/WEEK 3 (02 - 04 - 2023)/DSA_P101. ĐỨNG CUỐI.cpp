907d448c15ad8215a3a290d79140506d259c24f3
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-03 15:29:13 +0700

    Update and rename DSA_P101. DUNG CUOI.cpp to DSA_P101. ĐỨNG CUỐI.cpp

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P101. ĐỨNG CUỐI.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 3 (02 - 04 - 2023)/DSA_P101. ĐỨNG CUỐI.cpp
@@ -9,13 +9,13 @@ int main() {
     }
     string s2; cin >> s2;
     vector<string> v;
-    for (auto &x : st) {
+    for (auto &x: st) {
         if (x != s2) v.push_back(x);
 	}
-    for (auto &x : v) cout << x << ' ';
+    for (auto &x: v) cout << x << ' ';
     cout << s2 << '\n';
     while (next_permutation(v.begin(), v.end())) {
-        for (auto &x : v) cout << x << ' ';
+        for (auto &x: v) cout << x << ' ';
         cout << s2 << '\n';
     }
 }
