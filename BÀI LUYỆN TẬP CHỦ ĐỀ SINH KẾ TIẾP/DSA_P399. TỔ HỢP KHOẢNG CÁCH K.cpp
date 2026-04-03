98ad4af3ccbd840f198fbc5fc00c2e8c4458b07f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-03 15:09:22 +0700

    /

--- BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P399. TỔ HỢP KHOẢNG CÁCH K.cpp
+++ BÀI LUYỆN TẬP CHỦ ĐỀ SINH KẾ TIẾP/DSA_P399. TỔ HỢP KHOẢNG CÁCH K.cpp
@@ -2,29 +2,25 @@
 using namespace std;
 int n, k, a[100], ok;
 
-void kt(){
-	for(int i = 1; i <= k; i++) a[i] = i;
-}
-
 void sinh(){
-	int i = k;
-	while(i >= 1 && a[i] == n - k + i) i--;
-	if(!i) ok = 0;
-	else {
-		a[i]++;
-		for(int j = i + 1; j <= k; j++) a[j] = a[j - 1] + 1;
-	}
+    int i = k;
+    while(i >= 1 && a[i] == n - k + i) i--;
+    if(!i) ok = 0;
+    else {
+        a[i]++;
+        for(int j = i + 1; j <= k; j++) a[j] = a[j - 1] + 1;
+    }
 }
 
 int main(){
-	cin >> n >> k; kt(); ok = 1;
+    cin >> n >> k; ok = 1;
+    for(int i = 1; i <= k; i++) a[i] = i; 
     int cnt = 0;
     while(ok){
         if(cnt % k == 0){
             for(int i = 1; i <= k; i++) cout << a[i] << " ";
             cout << endl;
         }
-        sinh();
-        cnt++;
+        sinh(); cnt++;
     }
 }
