01375ebfc1aea0fecd853927bde7e466d4f203ab
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-04 14:53:58 +0700

    /

--- CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04021. DÃY XÂU NHỊ PHÂN.cpp
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04021. DÃY XÂU NHỊ PHÂN.cpp
@@ -1,15 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
 ll f[100];
 
-void pre(){
-	f[1] = 1; f[2] = 1;
-	for(int i = 3; i <= 92; i++) f[i] = f[i - 2] + f[i - 1];
-}
-
-char find(int n, ll k){
+char find(ll n, ll k){
 	if(n == 1) return '0';
 	if(n == 2) return '1';
 	if(k <= f[n - 2]) return find(n - 2,k);
@@ -18,10 +12,10 @@ char find(int n, ll k){
 
 int main(){
 	int t; cin >> t;
-	pre();
+	f[1] = 1; f[2] = 1;
+	for(ll i = 3; i <= 92;i++) f[i] = f[i - 1] + f[i - 2];
 	while(t--){
-		int n; ll k;
-		cin >> n >> k;
+		ll n, k;  cin >> n >> k;
 		cout << find(n, k) << endl;
 	}
 }
