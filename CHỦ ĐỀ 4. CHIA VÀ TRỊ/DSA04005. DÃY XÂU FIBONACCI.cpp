7b8fbc314afc9b0106b3f99ab6df85af13c1f9d2
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-04 09:50:25 +0700

    Update and rename DSA04005 - DAY XAU FIBONACCI.cpp to DSA04005. DÃY XÂU FIBONACCI.cpp

---
+++ CHỦ ĐỀ 4. CHIA VÀ TRỊ/DSA04005. DÃY XÂU FIBONACCI.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll f[100];
+
+char find(ll n, ll k){
+	if(n == 1) return 'A';
+	if(n == 2) return 'B';
+	if(k <= f[n - 2]) return find(n - 2,k);
+	else return find(n - 1, k - f[n - 2]);
+}
+
+int main(){
+	int t; cin >> t;
+	f[1] = 1; f[2] = 1;
+	for(ll i = 3; i <= 92;i++) f[i] = f[i - 1] + f[i - 2];
+	while(t--){
+		ll n, k;  cin >> n >> k;
+		cout << find(n, k) << endl;
+	}
+}
