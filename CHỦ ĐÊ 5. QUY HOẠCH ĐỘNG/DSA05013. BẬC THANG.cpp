48499638760aa3b0efdb6f7e8ad98e24b7e6ced9
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-10 09:09:36 +0700

    .

---
+++ CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSA05013. BẬC THANG.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int MOD = 1e9 + 7;
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int n, k; cin >> n >> k;
+		ll f[n + 1] = {0};
+		f[0] = 1;
+		for(int i = 1; i <= n; i++){
+			int x = min(i, k);
+			for(int j = 1; j <= x; j++){
+				f[i] += f[i - j];
+				f[i] %= MOD;
+			}
+		}
+		cout << f[n] << endl;
+	}
+}
