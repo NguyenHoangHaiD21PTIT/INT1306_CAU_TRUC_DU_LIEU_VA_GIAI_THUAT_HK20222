a758c73b98bff46917761002f30752a2d1d082de
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-10 09:14:49 +0700

    .

---
+++ CHỦ ĐÊ 5. QUY HOẠCH ĐỘNG/DSAKT110. NHÀ KHÔNG KỀ NHAU.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		int a[n + 2] = {0}, f[n + 2] = {0};
+		for(int i = 1; i <= n; i++) cin >> a[i];
+		f[0] = 0; f[1] = a[1];
+		for(int i = 2; i <= n; i++) f[i] = max(f[i - 1], f[i - 2] + a[i]);
+		cout << f[n] << endl;
+	}
+}
