6ce5e854075c94295c78522f39a408ff5b5451b5
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-13 17:23:29 +0700

    .

---
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/WEEK 2 (19 - 03 - 2023)/DSA_P042. DÃY SỐ ĐẸP.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n, a[55];
+        cin >> n;
+        for (int i = 0; i < n; i++) cin >> a[i];
+        int cnt = 0;
+        for(int i = 0; i < n - 1; i++){
+            int k = 2 * min(a[i], a[i + 1]);
+            while (max(a[i], a[i + 1]) > k){
+                cnt++;
+                k *= 2;
+            }
+        }
+        cout << cnt << endl;
+    }
+}
