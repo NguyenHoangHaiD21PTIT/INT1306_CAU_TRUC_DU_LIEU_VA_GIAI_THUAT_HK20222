92d407170477e4197514a45334cca080738974d1
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-17 07:37:49 +0700

    .

--- CHỦ ĐỀ 9. DUYỆT ĐỒ THỊ/DSA09027. KIỂM TRA ĐƯỜNG ĐI (DSU).cpp
+++ CHỦ ĐỀ 9. DUYỆT ĐỒ THỊ/DSA09027. KIỂM TRA ĐƯỜNG ĐI (DSU).cpp
@@ -4,41 +4,39 @@ using namespace std;
 int par[1005], sz[1005];
 
 int find(int x){
-	if(x==par[x]) return x;
+	if(x == par[x]) return x;
 	return par[x] = find(par[x]);
 }
 
 void Union(int a, int b){
 	a = find(a);
 	b = find(b);
-	if(a==b) return;
-	if(sz[a]<sz[b]) swap(a,b);
+	if(a == b) return;
+	if(sz[a] < sz[b]) swap(a,b);
 	par[b] = a;
-	sz[a]+=sz[b];
+	sz[a] += sz[b];
 }
 
 int main(){
-	int t;
-	cin>>t;
+	int t; cin >> t;
 	while(t--){
 		int n, m;
-		cin>>n>>m;
-		for(int i = 1;i<=n;i++){
+		cin >> n >> m;
+		for(int i = 1; i <= n; i++){
 			par[i] = i;
 			sz[i] = 1;
 		}
-		for(int i = 0;i<m;i++){
+		for(int i = 0; i < m; i++){
 			int x, y;
-			cin>>x>>y;
+			cin >> x >> y;
 			Union(x, y);
 		}
-		int q;
-		cin>>q;
+		int q; cin >> q;
 		while(q--){
 			int x, y;
-			cin>>x>>y;
-			if(find(x)==find(y)) cout<<"YES"<<endl;
-			else cout<<"NO"<<endl;
+			cin >> x >> y;
+			if(find(x) == find(y)) cout << "YES" << endl;
+			else cout << "NO" << endl;
 		}
 	}
 }
