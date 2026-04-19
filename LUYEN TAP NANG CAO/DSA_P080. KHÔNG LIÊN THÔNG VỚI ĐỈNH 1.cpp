29a0dcd042540152907d36355de963e3135b7da1
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-19 22:53:15 +0700

    .

--- LUYEN TAP NANG CAO/DSA_P080. KHÔNG LIÊN THÔNG VỚI ĐỈNH 1.cpp
+++ LUYEN TAP NANG CAO/DSA_P080. KHÔNG LIÊN THÔNG VỚI ĐỈNH 1.cpp
@@ -1,29 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 vector <int> adj[1005];
-int visited[1005];
+int vis[1005];
+
 void DFS(int x){
-    visited[x] = 1;
-    for(int i: adj[x]){
-        if(!visited[i]) DFS(i);
+    vis[x] = 1;
+    for (int i: adj[x]){
+        if (!vis[i]) DFS(i);
     }
 }
+
 int main(){
-    int v, e;
-    cin >> v >> e;
-    while(e--){
-        int x, y;
+    int n, m; cin >> n >> m;
+    while (m--){
+        int x, y; 
         cin >> x >> y;
         adj[x].push_back(y);
         adj[y].push_back(x);
     }
     DFS(1);
     int check = 0;
-    for(int i = 2; i <= v; i++){
-        if(!visited[i]){
+    for (int i = 2; i <= n; i++){
+        if (!vis[i]){
             check = 1;
             cout << i << endl;
         }
     }
-    if(!check) cout << 0;
+    if (!check) cout << 0;
 }
