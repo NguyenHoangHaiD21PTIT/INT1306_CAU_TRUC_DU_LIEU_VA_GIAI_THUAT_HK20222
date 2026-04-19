99fdaefda6bd3446a3150c0af40c8ad93297999d
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-19 23:22:57 +0700

    .

--- BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 12. 04/DSA_P062. SỐ LƯỢNG DẤU NGOẶC VUÔNG LỚN NHẤT.cpp
+++ BÀI KIỂM TRA SỐ 2 (75 PHÚT - 10 BÀI) (Ngăn xếp, Hàng đợi, Duyệt đồ thị)/DE THI NGAY 12. 04/DSA_P062. SỐ LƯỢNG DẤU NGOẶC VUÔNG LỚN NHẤT.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+bool check (char a, char b){
+    return (a == '(' && b == ')') || (a == '[' && b == ']');
+}
+
+int main(){
     string s; cin >> s;
     int cnt = 0, res = 0;
-    stack<pair<char, int>> st;
-    st.push({'*', 0}); // Để tránh stack rỗng
-    for (char i : s) {
-        if (!st.empty() && ((i == ')' && st.top().first == '(') || (i == ']' && st.top().first == '['))) {
-            // Đóng và có mở tương ứng --> Là dãy ngoặc đúng --> Xóa dấu mở ngoặc cùng cấp --> Thằng còn lại chính là thằng trước dấu mở ngoặc cùng cấp
-            //    ... i - 1     Mở đúng ... Đóng đúng
-            //  cnt trong stack     
+    stack <pair <char, int>>st; //{Loại ngoặc, Số dấu ngoặc vuông từ đầu chuỗi đến khi ký tự này được đưa vào stack}
+    st.push ({'*', 0});
+    for (char i: s){
+        if (!st.empty() && check (st.top().first, i)){
             st.pop();
-            if (!st.empty()) {
-                res = max(res, cnt - st.top().second);
-                // Như vậy, đỉnh stack bây giờ lưu dấu mở ngoặc và số dấu ngoặc vuông mở khi không xét đến dấu mở cùng cấp
-            }
-        } else { // Gặp mở hoặc đóng không khớp thì push vào
+            // Dấu đóng hiện tại khớp với dấu mở ở đỉnh stack -> Cặp ngoặc hợp lệ -> Lấy dấu mở đó ra khỏi stack.
+            if (!st.empty()) res = max (res, cnt - st.top().second);
+            // Sau khi pop, phần tử ở đỉnh stack chính là dấu ngoặc chưa được ghép cặp gần nhất nằm ngay trước dãy ngoặc hợp lệ mà ta vừa tìm được.
+            // - Số lượng dấu '[' NẰM BÊN TRONG dãy ngoặc hợp lệ vừa rồi sẽ bằng:
+            //   (Tổng số '[' từ đầu đến hiện tại) TRỪ ĐI (Tổng số '[' từ đầu tính đến cái Cột mốc biên kia).
+        } else {
             if (i == '[') cnt++;
-            st.push({i, cnt});
+            st.push ({i, cnt});
         }
     }
-    cout << res << endl;
+    cout << res;
 }
-// Bài này limit đã hạ xuống 1e3
