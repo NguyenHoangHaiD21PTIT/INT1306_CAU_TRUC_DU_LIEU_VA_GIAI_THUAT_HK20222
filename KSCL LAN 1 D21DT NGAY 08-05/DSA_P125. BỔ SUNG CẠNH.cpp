cb326b1e11c9a486d7241e1932688b63f710792a
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-19 22:41:38 +0700

    .

--- KSCL LAN 1 D21DT NGAY 08-05/DSA_P125. BỔ SUNG CẠNH.cpp
+++ KSCL LAN 1 D21DT NGAY 08-05/DSA_P125. BỔ SUNG CẠNH.cpp
@@ -1,40 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int par[100005], sze[100005];
+int par[100005], s[100005];
 
-int find(int x){
-	if(x == par[x]) return x;
-	return par[x] = find(par[x]);
+int find (int x){
+    if (x == par[x]) return x;
+    return par[x] = find(par[x]);
 }
 
-bool Union(int x, int y){
-	x = find(x); y = find(y);
-	if(x == y) return false;
-	if(sze[x] < sze[y]) swap(x, y);
-	par[y] = x;
-	sze[x] += sze[y];
-	return true;	
+bool join (int x, int y){
+    x = find(x); y = find(y);
+    if (x == y) return false;
+    if (s[x] < s[y]) swap(x, y);
+    par[y] = x;
+    s[x] += s[y];
+    return true;
 }
 
 int main(){
-	int V, E;
-    cin >> V >> E;
-    //Make_set
-	for(int i = 1;i <= V;i++){
-		par[i] = i;
-		sze[i] = 1;
-	}
-    while(E--){
+    int n, m; 
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++){
+        par[i] = i;
+        s[i] = 1;
+    }
+    while (m--){
         int x, y; cin >> x >> y;
-        Union(x, y);
+        join(x, y);
     }
-    int P1 = find(1), ans = 0;
-    for(int i = 2; i <= V; i++){
+    int ans = 0, P1 = find(1);
+    for (int i = 2; i <= n; i++){
         int PI = find(i);
-        if(PI != P1) ans = max(ans, sze[PI]);
+        if (PI != P1) ans = max (ans, s[PI]);
     }
-    cout << ans + sze[P1];
+    cout << ans + s[P1];
 }
 /*Idea: Nếu đồ thị liên thông thì số đỉnh tối đa đánh được là toàn bộ số đỉnh
 Ngược lại: Ta sẽ đánh dấu toàn bộ số đỉnh thuộc thành phần liên thông mà 1 thuộc về
