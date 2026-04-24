6805059210fd443fbf7212aa0a03a4e2740f4127
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-24 09:15:17 +0700

    .

--- BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P498. XOÁ XÂU CON ĐỐI XỨNG.cpp
+++ BAI THUC HANH SO 1 (Chủ đề Sinh kế tiếp, Quay lui - Nhánh cận, Giải thuật tham lam, Chia và trị, Quy hoạch động)/CONTEST DP (12 - 03 - 2023)/DSA_P498. XOÁ XÂU CON ĐỐI XỨNG.cpp
@@ -43,3 +43,45 @@ int main() {
         cout << f[0][n-1] << "\n";
     }
 }
+/* Các bạn có thể submit bài này tại: https://codeforces.com/problemset/problem/607/B 
+Lời giải: 
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 505; 
+int f[N][N]; 
+int s[N]; 
+
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) cin >> s[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) f[i][j] = 0;
+    }
+    for (int i = 0; i < n; i++) f[i][i] = 1;
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i + len - 1 < n; i++) {
+            int j = i + len - 1;
+            // Trường hợp 1 (Tự chủ): Xóa riêng s[i] mất 1 bước
+            f[i][j] = 1 + f[i + 1][j];
+            // Trường hợp 2 (Ghép với cái bên cạnh): 
+            if (s[i] == s[i + 1]) {
+                int tmp = 1; 
+                if (i + 2 <= j) tmp += f[i + 2][j];
+                f[i][j] = min(f[i][j], tmp);
+            } 
+            // Trường hợp 3 (Ghép s[i] với s[k] (k >= i+2))
+            for (int k = i + 2; k <= j; k++) {
+                if (s[i] == s[k]) {
+                    int left = 0;
+                    if (i + 1 <= k - 1) left = f[i + 1][k - 1];
+                    int right = 0;
+                    if (k + 1 <= j) right = f[k + 1][j];
+                    int tmp = left + right; 
+                    f[i][j] = min(f[i][j], tmp);
+                }
+            }
+        }
+    }
+    cout << f[0][n - 1];
+} */
