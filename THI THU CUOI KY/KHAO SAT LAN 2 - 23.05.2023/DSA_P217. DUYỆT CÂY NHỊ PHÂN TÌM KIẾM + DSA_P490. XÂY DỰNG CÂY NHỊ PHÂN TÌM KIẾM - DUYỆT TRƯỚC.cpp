8f6ab591024643d3930c311c739f03f735025a63
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-25 21:04:59 +0700

    .

--- THI THU CUOI KY/KHAO SAT LAN 2 - 23.05.2023/DSA_P217. DUYỆT CÂY NHỊ PHÂN TÌM KIẾM + DSA_P490. XÂY DỰNG CÂY NHỊ PHÂN TÌM KIẾM - DUYỆT TRƯỚC.cpp
+++ THI THU CUOI KY/KHAO SAT LAN 2 - 23.05.2023/DSA_P217. DUYỆT CÂY NHỊ PHÂN TÌM KIẾM + DSA_P490. XÂY DỰNG CÂY NHỊ PHÂN TÌM KIẾM - DUYỆT TRƯỚC.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 struct node {
     int d;
-    node* l, *r;
-    node(int x) {
+    node *l, *r;
+    node (int x){
         d = x;
         l = r = nullptr;
     }
 };
 
-node *ins(node *root, int u) {
-	if (root == NULL) root = new node(u);
-	else {
-		if (u < root->d) root->l = ins(root->l, u);
-		else root->r = ins(root->r, u);
-	}
-	return root;
+node *ins (node *root, int u){
+    if (!root) root = new node (u);
+    else {
+        if (u < root -> d) root -> l = ins (root -> l, u);
+        else root -> r = ins (root -> r, u);
+    }
+    return root;
 }
 
-void pre(node *root) {
-	if (root == NULL) return;
-	cout << root->d << " ";
-	pre(root->l);
-	pre(root->r);
+void pre (node *root){
+    if (!root) return;
+    cout << root -> d << " ";
+    pre (root -> l);
+    pre (root -> r);
 }
 
 int main(){
-	int t; cin >> t;
-	while (t--) {
-		node *root = NULL;
-		int n; cin >> n;
-		for (int i = 0; i < n; i++) {
-			int x; cin >> x;
-			root = ins(root, x);
-		}
-		pre(root);
-		cout << endl;
-	}
+    int t; cin >> t;
+    while (t--){
+        node *root = NULL;
+        int n; cin >> n;
+        while (n--){
+            int x; cin >> x;
+            root = ins (root, x);
+        }
+        pre (root);
+        cout << endl;
+    }
 }
