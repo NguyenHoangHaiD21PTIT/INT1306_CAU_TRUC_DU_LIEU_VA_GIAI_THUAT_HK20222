9699b3aa8660636932f3f35bcdb769e3f5298d16
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-25 21:07:07 +0700

    .

--- KSCL LAN 1 D21DT NGAY 08-05/DSA_P222. PHÉP DUYỆT RIGHT LEVEL ODER TRÊN CÂY NHỊ PHÂN TÌM KIẾM.cpp
+++ KSCL LAN 1 D21DT NGAY 08-05/DSA_P222. PHÉP DUYỆT RIGHT LEVEL ODER TRÊN CÂY NHỊ PHÂN TÌM KIẾM.cpp
@@ -2,57 +2,56 @@
 using namespace std;
 
 struct node {
-    int d;
-    node* l, *r;
-    node(int x) {
-        d = x;
-        l = r = nullptr;
+    int d; 
+    node *l, *r;
+    node (int u){
+        d = u; 
+		l = r = nullptr;
     }
 };
 
-node *ins(node *root, int u) {
-	if (root == NULL) root = new node(u);
-	else {
-		if (u < root->d) root->l = ins(root->l, u);
-		else root->r = ins(root->r, u);
-	}
-	return root;
+node *ins (node *root, int u){
+    if (!root) root = new node (u);
+    else {
+        if (u < root -> d) root -> l = ins (root -> l, u);
+        else root -> r = ins (root -> r, u);
+    }
+    return root;
 }
 
-void sp(node* rt) {
-    if (!rt) return;
-    queue<node *> q;
-    vector<vector<int>> res;
-    q.push(rt);
-    while (!q.empty()) {
+void lv (node *root){
+    if (!root) return;
+    queue <node *> q;
+    q.push(root);
+    vector <vector <int>> res;
+    while (!q.empty()){
         // Ý tưởng: Xét hàng đợi theo từng tầng. Mỗi tầng lấy lần lượt ra rồi cho các con vào hàng đợi. 
-        int c = q.size(); //Số node tầng đang xét
-        vector<int> v; //Mảng lưu các node tầng đang xét
-        while (c--) {
-            node * t = q.front(); q.pop();
-            v.push_back(t->d);
-            if (t->l) q.push(t->l);
-            if (t->r) q.push(t->r);
+        int c = q.size();//Số node tầng đang xét
+        vector <int> v;//Mảng lưu các node tầng đang xét
+        while (c--){
+            node *t = q.front(); q.pop();
+            v.push_back(t -> d);
+            if (t -> l) q.push (t -> l);
+            if (t -> r) q.push (t -> r);
         }
         res.push_back(v);
     }
-    // Nếu BFS thông thường thì không cần q.size(). Cứ lấy phần tử đằng trước, pop rồi cho 2 con vào qe
-    for (int i = 0; i < res.size(); i++) {
-        for (int j = res[i].size() - 1; j >= 0; j--) cout << res[i][j] << " ";
+    for (auto x: res){
+        reverse(x.begin(), x.end());
+        for (int y: x) cout << y << " ";
     }
 }
 
-int main() {
-    int t, n, x;
-    cin >> t;
-    while (t--) {
-        node* rt = NULL;
-        cin >> n;
-        while (n--) {
-            cin >> x;
-            rt = ins(rt, x);
+int main(){
+    int t; cin >> t;
+    while (t--){
+        node *root = NULL;
+        int n; cin >> n;
+        while (n--){
+            int x; cin >> x;
+            root = ins (root, x);
         }
-        sp(rt);
-        cout << "\n";
+        lv (root);
+        cout << endl;
     }
 }
