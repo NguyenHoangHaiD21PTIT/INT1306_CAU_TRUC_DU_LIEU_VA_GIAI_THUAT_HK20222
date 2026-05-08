380cd1b10a07f2bf280088c7d25b6c9427985771
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-05-08 08:40:53 +0700

    .

--- BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSAKT121. DUYỆT ĐỒ THỊ.cpp
+++ BÀI THỰC HÀNH SỐ 2 (Chủ đề Ngăn xếp, Hàng đợi, Duyệt đồ thị và Cây nhị phân) (Thi ngày 07-05-2023)/DSAKT121. DUYỆT ĐỒ THỊ.cpp
@@ -1,28 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector <int> ke[1005];
-int visited[1005] = {};
+vector <int> adj[1005];
+int vis[1005] = {};
 
 void DFS(int x){
-	visited[x] = 1;
+	vis[x] = 1;
 	cout << x << " ";
-	for(int i: ke[x]){
-		if(!visited[i]) DFS(i);
+	for(int i: adj[x]){
+		if(!vis[i]) DFS(i);
 	}
 }
 int main(){
 	int t; cin >> t;
 	while(t--){
-		memset(visited, 0, sizeof(visited));
-		int dinh, canh, v;
-		cin >> dinh >> canh >> v;
-		for(int i = 1; i <= dinh; i++) ke[i].clear();
-		while(canh--){
-			int dau, cuoi;
-			cin >> dau >> cuoi;
-			ke[dau].push_back(cuoi);
-			ke[cuoi].push_back(dau);
+		memset(vis, 0, sizeof(vis));
+		int n, m, v;
+		cin >> n >> m >> v;
+		for(int i = 1; i <= n; i++) adj[i].clear();
+		while(m--){
+			int x, y;
+			cin >> x >> y;
+			adj[x].push_back(y);
+			adj[y].push_back(x);
 		}
 		cout << "DFS(" << v << ") = ";
 		DFS(v);
