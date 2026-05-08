9b1781969b8ecc6f87cc8874b9e5e7e360b687cb
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-05-08 07:27:35 +0700

    .

--- CHỦ ĐỀ 1. SINH KẾ TIẾP/DSA01008. XÂU NHỊ PHÂN CÓ K BIT 1.cpp
+++ CHỦ ĐỀ 1. SINH KẾ TIẾP/DSA01008. XÂU NHỊ PHÂN CÓ K BIT 1.cpp
@@ -25,7 +25,7 @@ int main(){
 		for(int i = 0; i < n; i++) a[i] = 0; 
 		while(ok){
 			if(check()){
-				for(int i = 0; i < n; i++) cout << a[i] << " ";
+				for(int i = 0; i < n; i++) cout << a[i];
 				cout << endl;
 			}
 			sinh();
